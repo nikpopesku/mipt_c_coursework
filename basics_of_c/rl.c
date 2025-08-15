f17acc79a0b27467acfe5a2d0af15415a1bb4fa2
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-15 15:14:54 +0800

    basics_of_c/ee.c

---
+++ basics_of_c/rl.c
@@ -0,0 +1,33 @@
+#include <assert.h>
+#include <stdio.h>
+
+unsigned long long gcd(long long x, long long y)
+{
+    if (x < 0) x = -x;
+    if (y < 0) y = -y;
+
+    while (y != x)
+    {
+        if (y > x)
+        {
+            y -= x;
+        }
+        else
+        {
+            x -= y;
+        }
+    }
+
+    return y;
+}
+
+int main()
+{
+    long long x = 0, y = 0;
+
+    const int res = scanf("%lld %lld", &x, &y);
+    assert(res == 2);
+    const long long g = gcd(x, y);
+    printf("%llu\n", g);
+    return 0;
+}

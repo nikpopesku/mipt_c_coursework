1e97decf4566c1f60986f3db95fbf224934a5ee4
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-15 15:12:05 +0800

    basics_of_c/problem_cf.c

---
+++ basics_of_c/problem_cf.c
@@ -0,0 +1,36 @@
+#include <assert.h>
+#include <stdio.h>
+
+long long calc(long long x, long long y, long long response[])
+{
+    long long index = -1;
+
+    while (x % y != 0)
+    {
+        response[++index] = x / y;
+        const int tmp = response[index] > 0 ? x - response[index] * y : x;
+        x = y;
+        y = tmp;
+    }
+
+    response[++index] = x / y;
+
+    return index;
+}
+
+int main()
+{
+    long long x = 0, y = 0;
+
+    const int res = scanf("%lld %lld", &x, &y);
+    assert(res == 2);
+    long long response[100];
+    long long index = calc(x, y, response);
+
+    for (long long i = 0; i <= index; ++i)
+    {
+        printf("%lld ", response[i]);
+    }
+
+    return 0;
+}

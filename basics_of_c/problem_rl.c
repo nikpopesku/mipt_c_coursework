3383e12f5127ef6c2bd28aa7bc94c8f20cf8b41b
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-15 13:40:23 +0800

    basics_of_c/problem_rl.c

--- basics_of_c/problem_rl.c
+++ basics_of_c/problem_rl.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 
 unsigned long long gcd(unsigned long long x, unsigned long long y) {
-    unsigned long long q;
     if (y > x)
         return gcd(y, x);
     assert (y > 0);
-    q = x % y;
+    const unsigned long long q = x % y;
     if (q == 0)
         return y;
     return gcd(y, q);
@@ -15,7 +14,7 @@ unsigned long long gcd(unsigned long long x, unsigned long long y) {
 int main() {
     unsigned long long x = 0, y = 0, g;
 
-    int res = scanf("%llu %llu", &x, &y);
+    const int res = scanf("%llu %llu", &x, &y);
     assert(res == 2);
     g = gcd(x, y);
     printf("%llu\n", g);

4451aee5dd75171c27d32670152aa39493cba8d4
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-16 13:51:25 +0800

    basics_of_c/de_diophantine equations.c

---
+++ basics_of_c/de_diophantine equations.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+    int a, b, c;
+
+    const int res = scanf("%d %d %d", &a, &b, %c);
+    assert(res == 2);
+
+
+    // printf("%lld %lld %lld", a, b, gcd);
+}

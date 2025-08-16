6dcba9631216a718a349bf7784fcaeae72083a01
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-16 12:21:56 +0800

    basics_of_c/ee_extended_euclid.c

--- basics_of_c/ee_extended_euclid.c
+++ basics_of_c/ee_extended_euclid.c
@@ -20,6 +20,7 @@ long long ee_gcd(long long koef[1000][4])
 int main()
 {
     long long x = 0, y = 0;
+    int swapped = 0;
 
     const int res = scanf("%lld %lld", &x, &y);
     assert(res == 2);
@@ -32,6 +33,7 @@ int main()
         const long long tmp = x;
         x = y;
         y = tmp;
+        swapped = 1;
     }
 
     koef[0][0] = x;
@@ -42,6 +44,8 @@ int main()
     koef[1][3] = 1;
 
     const long long index = ee_gcd(koef);
+    const long long a = swapped ? koef[index][3] : koef[index][2];
+    const long long b = swapped ? koef[index][2] : koef[index][3];
 
-    printf("%lld %lld %lld", koef[index][2], koef[index][3], koef[index][0]);
+    printf("%lld %lld %lld", a, b, koef[index][0]);
 }

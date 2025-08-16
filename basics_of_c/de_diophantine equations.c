fa7ae11eb067e27afba8a1af75ca93b34f36bc64
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-16 14:10:58 +0800

    basics_of_c/de_diophantine equations.c

--- basics_of_c/de_diophantine equations.c
+++ basics_of_c/de_diophantine equations.c
@@ -2,7 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int de_solution(int coef[1000][4])
+{
+    int index = 2;
 
+    while (coef[index - 2][0] % coef[index - 1][0] != 0)
+    {
+        coef[index][0] = coef[index - 2][0] % coef[index - 1][0];
+        coef[index][1] = coef[index - 2] / coef[index - 1];
+        coef[index][2] = coef[index - 2][2] - coef[index][1] * coef[index - 1][2];
+        coef[index][3] = coef[index - 2][3] - coef[index][1] * coef[index - 1][3];
+        ++index;
+    }
+
+    return index - 1;
+}
 
 int main()
 {
@@ -10,6 +24,24 @@ int main()
 
     const int res = scanf("%d %d %d", &a, &b, %c);
     assert(res == 2);
+
+    int coef[1000][4];
+
+    coef[0][0] = abs(a);
+    coef[0][2] = 1;
+    coef[0][3] = 0;
+    coef[1][0] = abs(b);
+    coef[1][2] = 0;
+    coef[1][3] = 1;
+
+    const int index = de_solution(coef);
+
+    if (coef[index][1] % c != 0)
+    {
+        printf("NONE");
+
+        return 0;
+    }
 
 
     // printf("%lld %lld %lld", a, b, gcd);

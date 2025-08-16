abc2d316b779a87b2053bf8abf19cb97935ae0c0
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-16 12:51:45 +0800

    basics_of_c/ee_extended_euclid.c

--- basics_of_c/ee_extended_euclid.c
+++ basics_of_c/ee_extended_euclid.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 long long ee_gcd(long long koef[1000][4])
 {
@@ -20,39 +21,22 @@ long long ee_gcd(long long koef[1000][4])
 int main()
 {
     long long x = 0, y = 0;
-    int swapped = 0;
-    int minus = 0;
 
     const int res = scanf("%lld %lld", &x, &y);
     assert(res == 2);
 
     long long koef[1000][4];
-    if (x < y && (x > 0 || y > 0))
-    {
-        const long long tmp = x;
-        x = y;
-        y = tmp;
-        swapped = 1;
-    }
-
-    if (x < 0 && y < 0)
-    {
-        minus = 1;
-    }
-
-    koef[0][0] = x;
+    koef[0][0] = llabs(x);
     koef[0][2] = 1;
     koef[0][3] = 0;
-    koef[1][0] = y;
+    koef[1][0] = llabs(y);
     koef[1][2] = 0;
     koef[1][3] = 1;
 
     const long long index = ee_gcd(koef);
-    long long a = swapped ? koef[index][3] : koef[index][2];
-    long long b = swapped ? koef[index][2] : koef[index][3];
-    a = minus ? -a : a;
-    b = minus ? -b : b;
-    long long gcd = minus ? -koef[index][0] : koef[index][0];
+    const long long a = x < 0 ? -koef[index][2] : koef[index][2];
+    const long long b = y < 0 ? -koef[index][3] : koef[index][3];
+    const long long gcd = koef[index][0];
 
     printf("%lld %lld %lld", a, b, gcd);
 }

a3cdeee685b195beb9858ad1c4ce9a9e6a4eec8e
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-18 11:52:44 +0800

    basics_of_c/fs_factorial_system.c

--- basics_of_c/fs_factorial_system.c
+++ basics_of_c/fs_factorial_system.c
@@ -3,34 +3,34 @@
 
 int main()
 {
-    unsigned int x, y;
+    unsigned int x;
 
     const int res = scanf("%u", &x);
     assert(res == 1);
     int response[100];
     int index = 0;
-    unsigned int current_y = 1;
-    unsigned int current_factorial = 1;
+    unsigned int y = 1;
+    unsigned int factorial = 1;
 
 
     while (x >= 0)
     {
-        if (x / current_factorial <= y)
+        if (x / factorial <= y)
         {
-            response[index] = x / current_y;
-            x = x % current_y;
-            current_factorial /= current_y;
-            current_y -= 1;
+            response[index] = x / factorial;
+            x = x % factorial;
+            factorial /= y;
+            y -= 1;
             ++index;
-            if (current_y == 0)
+            if (y == 0)
             {
                 break;
             }
         }
         else
         {
-            current_factorial *= current_y + 1;
-            current_y *= current_y + 1;
+            factorial *= y + 1;
+            y += 1;
         }
     }
 

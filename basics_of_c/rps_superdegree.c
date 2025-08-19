3c6a667adce401672ef5ce587faee36a7b3844a0
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-19 11:03:50 +0800

    basics_of_c/rps_superdegree.c

--- basics_of_c/rps_superdegree.c
+++ basics_of_c/rps_superdegree.c
@@ -3,39 +3,8 @@
 
 int main()
 {
-    unsigned int x;
+    int a, b, n;
 
-    const int res = scanf("%u", &x);
-    assert(res == 1);
-    int response[100];
-    int index = 0;
-    unsigned int y = 1;
-    unsigned int factorial = 1;
-
-
-    while (x >= 0)
-    {
-        if (x / factorial <= y)
-        {
-            response[index] = x / factorial;
-            x = x % factorial;
-            factorial /= y;
-            y -= 1;
-            ++index;
-            if (y == 0)
-            {
-                break;
-            }
-        }
-        else
-        {
-            factorial *= y + 1;
-            y += 1;
-        }
-    }
-
-    for (int i = 0; i < index; ++i)
-    {
-        printf("%u.", response[i]);
-    }
+    const int res = scanf("%d %d %d", &a, &b, &n);
+    assert(res == 3);
 }

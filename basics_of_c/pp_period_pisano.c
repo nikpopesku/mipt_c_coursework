dcecb568d54d80f94c41321f56b80346645024a4
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-23 12:58:34 +0800

    basics_of_c/pp_period_pisano.c

--- basics_of_c/pp_period_pisano.c
+++ basics_of_c/pp_period_pisano.c
@@ -7,24 +7,25 @@ void solve(const int x, const int m)
     int second_value = 1 % m;
     int pisano_response = 0;
     int fib_response = 0;
+    int value = 0;
 
     do
     {
-        const int value = (first_value + second_value) % m;
+        value = (first_value + second_value) % m;
         second_value = value;
         first_value = second_value;
         ++pisano_response;
     }
-    while (!(first_value == 0 && second_value == 1));
+    while (!(value == 1 && second_value == 0));
 
     printf("%d %d", fib_response, pisano_response);
 }
 
 int main()
 {
-    unsigned int x, m;
+    int x, m;
 
-    const int res = scanf("%u %u", &x, &m);
+    const int res = scanf("%d %d", &x, &m);
     assert(res == 2);
 
     solve(x, m);

f9a3ebde4a7347368137c813c2d6a921c6c6c3b8
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-24 21:45:52 +0800

    basics_of_c/mf_matrix_fibonacci.c

--- basics_of_c/mf_matrix_fibonacci.c
+++ basics_of_c/mf_matrix_fibonacci.c
@@ -14,22 +14,23 @@ Matrix solve(const int x00, const int x01, const int x10, const int x11, const i
     if (n == 1)
     {
         Matrix response;
-        response.x00 = x00;
-        response.x10 = x10;
-        response.x01 = x01;
-        response.x11 = x11;
+        response.x00 = x00 % m;
+        response.x10 = x10 % m;
+        response.x01 = x01 % m;
+        response.x11 = x11 % m;
 
         return response;
     }
+
     if (n % 2 == 0)
     {
         const Matrix value = solve(x00, x01, x10, x11, n / 2, m);
 
         Matrix response;
-        response.x00 = value.x00 * value.x00 + value.x01 * value.x10;
-        response.x01 = value.x00 * value.x01 + value.x01 * value.x11;
-        response.x10 = value.x10 * value.x00 + value.x11 * value.x10;
-        response.x11 = value.x10 * value.x01 + value.x11 * value.x11;
+        response.x00 = (value.x00 * value.x00 % m + value.x01 * value.x10 % m) % m;
+        response.x01 = (value.x00 * value.x01 % m + value.x01 * value.x11 % m) % m;
+        response.x10 = (value.x10 * value.x00 % m + value.x11 * value.x10 % m) % m;
+        response.x11 = (value.x10 * value.x01 % m + value.x11 * value.x11 % m) % m;
 
         return response;
     }
@@ -37,10 +38,10 @@ Matrix solve(const int x00, const int x01, const int x10, const int x11, const i
     const Matrix value = solve(x00, x01, x10, x11, n - 1, m);
 
     Matrix response;
-    response.x00 = value.x00 * value.x00 + value.x01 * value.x10;
-    response.x10 = x10;
-    response.x01 = x01;
-    response.x11 = x11;
+    response.x00 = (x00 * value.x00 % m + x01 * value.x10 % m) % m;
+    response.x01 = (x00 * value.x01 % m + x01 * value.x11 % m) % m;
+    response.x10 = (x10 * value.x00 % m + x11 * value.x10 % m) % m;
+    response.x11 = (x10 * value.x01 % m + x11 * value.x11 % m) % m;
 
     return response;
 }

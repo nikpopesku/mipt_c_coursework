1e995d4de96e6799dba63e1cda49c99df9812f0f
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-24 17:21:39 +0800

    basics_of_c/mf_matrix_fibonacci.c

--- basics_of_c/mf_matrix_fibonacci.c
+++ basics_of_c/mf_matrix_fibonacci.c
@@ -1,18 +1,18 @@
 #include <assert.h>
 #include <stdio.h>
 
-void solve(int x)
+void solve(int x00, int x01, int x10, int x11, int n, int m)
 {
 }
 
 int main()
 {
-    int x = 0;
+    int x00, x01, x10, x11, n, m;
 
-    const int res = scanf("%d", &x);
+    const int res = scanf("%d %d %d %d %d %d", &x00, &x01, &x10, &x11, n, m);
     assert(res == 6);
 
-    solve(x);
+    solve(x00, x01, x10, x11, n, m);
 
     return 0;
 }

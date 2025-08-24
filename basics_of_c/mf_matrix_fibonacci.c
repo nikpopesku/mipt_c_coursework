5081f5a4d223b4d503a951257430ad6e3c262105
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-24 17:17:59 +0800

    basics_of_c/mf_matrix_fibonacci.c

---
+++ basics_of_c/mf_matrix_fibonacci.c
@@ -0,0 +1,18 @@
+#include <assert.h>
+#include <stdio.h>
+
+void solve(int x)
+{
+}
+
+int main()
+{
+    int x = 0;
+
+    const int res = scanf("%d", &x);
+    assert(res == 6);
+
+    solve(x);
+
+    return 0;
+}

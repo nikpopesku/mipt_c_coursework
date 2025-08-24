86d076c57a32e363e266ffb08370179e2d266483
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-24 20:48:44 +0800

    basics_of_c/mf_matrix_fibonacci.c

--- basics_of_c/mf_matrix_fibonacci.c
+++ basics_of_c/mf_matrix_fibonacci.c
@@ -1,7 +1,7 @@
 #include <assert.h>
 #include <stdio.h>
 
-void solve(int& x00, int& x01, int& x10, int& x11, const int n, const int m)
+void solve(int* x00, int* x01, int* x10, int* x11, const int n, const int m)
 {
     if (n == 1)
     {
@@ -16,15 +16,15 @@ void solve(int& x00, int& x01, int& x10, int& x11, const int n, const int m)
     {
         solve(x00, x01, x10, x11, n - 1, m);
     }
-    const int updated_x00 = (x00 * x00 % m + x01 * x10 % m) % m;
-    const int updated_x01 = (x00 * x01 % m + x01 * x11 % m) % m;
-    const int updated_x10 = (x10 * x00 % m + x11 * x10 % m) % m;
-    const int updated_x11 = (x10 * x01 % m + x11 * x11 % m) % m;
+    const int updated_x00 = ((*x00) * (*x00) % m + (*x01) * (*x10) % m) % m;
+    const int updated_x01 = ((*x00) * (*x01) % m + (*x01) * (*x11) % m) % m;
+    const int updated_x10 = ((*x10) * (*x00) % m + (*x11) * (*x10) % m) % m;
+    const int updated_x11 = ((*x10) * (*x01) % m + (*x11) * (*x11) % m) % m;
 
-    x00 = updated_x00;
-    x01 = updated_x01;
-    x10 = updated_x10;
-    x11 = updated_x11;
+    *x00 = updated_x00;
+    *x01 = updated_x01;
+    *x10 = updated_x10;
+    *x11 = updated_x11;
 }
 
 int main()
@@ -39,7 +39,7 @@ int main()
     x10 = x10 % m;
     x11 = x11 % m;
 
-    solve(x00, x01, x10, x11, n, m);
+    solve(&x00, &x01, &x10, &x11, n, m);
 
     printf("%d %d %d %d", x00, x01, x10, x11);
 

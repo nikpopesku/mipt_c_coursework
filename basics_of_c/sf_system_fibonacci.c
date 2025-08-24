3ecf8df727bd72c4442c0ce0a6a1c33c3f4a49f3
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-24 12:42:45 +0800

    basics_of_c/sf_system_fibonacci.c

--- basics_of_c/sf_system_fibonacci.c
+++ basics_of_c/sf_system_fibonacci.c
@@ -1,14 +1,14 @@
 #include <assert.h>
 #include <stdio.h>
 
-void solve(int x)
+void solve(int long long x)
 {
-    int f[10000];
+    int long long f[10000];
     int coef[10000];
     f[0] = 1;
     f[1] = 1;
     int counter = 1;
-    int max_counter = 1;
+    int max_counter;
     coef[0] = 0;
     coef[1] = 0;
 
@@ -41,9 +41,9 @@ void solve(int x)
 
 int main()
 {
-    int x = 0;
+    int long long x = 0;
 
-    const int res = scanf("%d", &x);
+    const int res = scanf("%lld", &x);
     assert(res == 1);
 
     solve(x);

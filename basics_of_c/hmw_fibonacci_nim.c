2282f42541afd82b0609738ae094364d60e330cb
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-25 14:36:26 +0800

    basics_of_c/hmw_fibonacci_nim.c

--- basics_of_c/hmw_fibonacci_nim.c
+++ basics_of_c/hmw_fibonacci_nim.c
@@ -1,9 +1,19 @@
 #include <assert.h>
 #include <stdio.h>
 
+int next_turn(const int total, const int possible)
+{
+    int last = 0, penultimate = 1;
 
-int next_turn(int total, int possible) {
-    return 5;
+    while (last + penultimate <= possible)
+    {
+        const int value = last + penultimate;
+        last = penultimate;
+        penultimate = value;
+    }
+
+
+    return total - penultimate;
 }
 
 int main()
@@ -15,7 +25,7 @@ int main()
 
     next_turn(total, possible);
 
-    int next = next_turn(total, possible);
+    const int next = next_turn(total, possible);
     printf("%d\n", next);
 
     return 0;

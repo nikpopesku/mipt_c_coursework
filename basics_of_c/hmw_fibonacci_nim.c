39219fbda6339f104af358e4c5f103d82bdbb3fe
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-25 14:56:48 +0800

    basics_of_c/hmw_fibonacci_nim.c

--- basics_of_c/hmw_fibonacci_nim.c
+++ basics_of_c/hmw_fibonacci_nim.c
@@ -1,19 +1,54 @@
 #include <assert.h>
 #include <stdio.h>
 
+void factorial_system(int total, int factorial_array[1000])
+{
+    int index = 0;
+    int step = 1;
+    int factorial = 1;
+
+    while (total >= 0)
+    {
+        if (total / factorial <= step)
+        {
+            factorial_array[index] = total / factorial;
+            total = total % factorial;
+            factorial /= step;
+            step -= 1;
+            ++index;
+            if (step == 0)
+            {
+                break;
+            }
+        }
+        else
+        {
+            factorial *= step + 1;
+            step += 1;
+        }
+    }
+}
+
 int next_turn(const int total, const int possible)
 {
-    int last = 0, penultimate = 1;
+    int factorial[1000];
+    for (int i = 0; i < 1000; ++i) factorial[i] = 0;
+    factorial_system(total, factorial);
 
-    while (last + penultimate <= possible)
+    for (int i = 0; i < 1000; ++i)
     {
-        const int value = last + penultimate;
-        last = penultimate;
-        penultimate = value;
+        if (factorial[i] == 1)
+        {
+            if (i <= possible)
+            {
+                return i;
+            }
+
+            return 1;
+        }
     }
 
-
-    return total - penultimate;
+    return 1;
 }
 
 int main()

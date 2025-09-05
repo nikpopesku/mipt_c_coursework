1d20ea6fb9959a8525b016247b06f73e0405480c
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-05 11:31:55 +0800

    time_and_memory/se_sieve of eratosthenes.c

--- time_and_memory/se_sieve of eratosthenes.c
+++ time_and_memory/se_sieve of eratosthenes.c
@@ -1,19 +1,42 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+struct sieve_t {
+    unsigned size;
+    unsigned char *sieve;
+};
 
-int long long solve(const int long long N)
+int long long solve(const int long long n)
 {
+    struct sieve_t s = init_sieve(100);
+    assert(s.sieve != NULL && s.size > 0);
+}
+
+sieve_t init_sieve(const int long long n)
+{
+
+}
+
+void free_sieve(struct sieve_t *s) {
+    free(s->sieve);
+    s->sieve = 0;
+    s->size = 0;
+}
+
+unsigned is_prime(struct sieve_t s, unsigned n) {
+    assert(n < s.size);
+    return (s.sieve[n] == 1) ? 0 : 1;
 }
 
 int main()
 {
-    int long long N = 0;
+    int long long n = 0;
 
-    const int res = scanf("%lld", &N);
+    const int res = scanf("%lld", &n);
     assert(res == 1);
 
-    const int long long number = solve(N);
+    const int long long number = solve(n);
 
     printf("%lld", number);
 

78a453ca62eaccd3a1be5e7af14813cfc7ace1ec
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-05 11:25:28 +0800

    time_and_memory/se_sieve of eratosthenes.c

---
+++ time_and_memory/se_sieve of eratosthenes.c
@@ -0,0 +1,21 @@
+#include <assert.h>
+#include <stdio.h>
+
+
+int long long solve(const int long long N)
+{
+}
+
+int main()
+{
+    int long long N = 0;
+
+    const int res = scanf("%lld", &N);
+    assert(res == 1);
+
+    const int long long number = solve(N);
+
+    printf("%lld", number);
+
+    return 0;
+}

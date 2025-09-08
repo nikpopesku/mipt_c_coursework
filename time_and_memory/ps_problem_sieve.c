ab69a8245937c810841921cd7816d86911245d61
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-08 13:52:08 +0800

    time_and_memory/ps_problem_sieve.c

--- time_and_memory/ps_problem_sieve.c
+++ time_and_memory/ps_problem_sieve.c
@@ -1,6 +1,18 @@
 #include "u_template.h"
 #include <stdio.h>
 #include <assert.h>
+#include <math.h>
+#include <assert.h>
+#include <stdlib.h>
+
+void construct_sieve(sieve_t* sv, int N)
+{
+    int value = log2(N);
+    int length = N * (value + log2(value));
+    sv->n = length;
+    char* a = calloc(length, sizeof(char));
+    sv->s = a;
+}
 
 void fill_sieve(sieve_t* sv)
 {
@@ -10,6 +22,13 @@ void fill_sieve(sieve_t* sv)
 int nth_prime(sieve_t* sv, int N)
 {
     return N + 4;
+}
+
+void free_sieve(sieve_t* s)
+{
+    free(s->s);
+    s->s = 0;
+    s->n = 0;
 }
 
 int main()
@@ -24,6 +43,8 @@ int main()
     const int number = nth_prime(&sieve, n);
 
     printf("%d", number);
+
+    free_sieve(&sieve);
 
     return 0;
 }

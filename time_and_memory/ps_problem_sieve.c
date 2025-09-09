663164210f346dbc5ddee07d917721666164e214
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-09 11:47:30 +0800

    time_and_memory/runit.c

--- time_and_memory/ps_problem_sieve.c
+++ time_and_memory/ps_problem_sieve.c
@@ -1,17 +1,4 @@
 #include "u_template.h"
-#include <stdio.h>
-#include <assert.h>
-#include <math.h>
-#include <assert.h>
-#include <stdlib.h>
-
-void construct_sieve(sieve_t* sv, const int N)
-{
-    const int value = log2(N);
-    sv->n = N * (value + log2(value));
-    char* a = calloc(sv->n, sizeof(char));
-    sv->s = a;
-}
 
 void fill_sieve(sieve_t* sv)
 {
@@ -45,30 +32,4 @@ int nth_prime(sieve_t* sv, int N)
     }
 
     return -1;
-}
-
-void free_sieve(sieve_t* s)
-{
-    free(s->s);
-    s->s = 0;
-    s->n = 0;
-}
-
-int main()
-{
-    int n = 0;
-
-    const int res = scanf("%d", &n);
-    assert(res == 1);
-
-    sieve_t sieve;
-    construct_sieve(&sieve, n);
-    fill_sieve(&sieve);
-    const int number = nth_prime(&sieve, n);
-
-    printf("%d", number);
-
-    free_sieve(&sieve);
-
-    return 0;
 }
---
+++ time_and_memory/runit.c
@@ -0,0 +1,42 @@
+#include "u_template.h"
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+#include <assert.h>
+#include <stdlib.h>
+#include "ps_problem_sieve.c"
+
+void construct_sieve(sieve_t* sv, const int N)
+{
+    const int value = log2(N);
+    sv->n = N * (value + log2(value));
+    char* a = calloc(sv->n, sizeof(char));
+    sv->s = a;
+}
+
+
+void free_sieve(sieve_t* s)
+{
+    free(s->s);
+    s->s = 0;
+    s->n = 0;
+}
+
+int main()
+{
+    int n = 0;
+
+    const int res = scanf("%d", &n);
+    assert(res == 1);
+
+    sieve_t sieve;
+    construct_sieve(&sieve, n);
+    fill_sieve(&sieve);
+    const int number = nth_prime(&sieve, n);
+
+    printf("%d", number);
+
+    free_sieve(&sieve);
+
+    return 0;
+}

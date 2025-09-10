7a815488e613406ff7e5e4fdb9d6a712c63ad55c
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-10 12:03:58 +0800

    time_and_memory/gf_generation_formula.c

--- time_and_memory/gf_generation_formula.c
+++ time_and_memory/gf_generation_formula.c
@@ -1,11 +1,26 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+struct sieve_t
+{
+    int long long n;
+    char* s;
+};
 
 int a, b, max_sequence_length;
 
+void construct_sieve(struct sieve_t* sieve)
+{
+    sieve->n = 10000;
+    char* a = calloc(sieve->n, sizeof(char));
+    sieve->s = a;
+}
+
 void solve(int N)
 {
-
+    struct sieve_t sieve;
+    construct_sieve(sieve);
 }
 
 int main()

1655cb3489361c52c32ce6d1d9100d5acca1048a
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-10 12:20:16 +0800

    time_and_memory/gf_generation_formula.c

--- time_and_memory/gf_generation_formula.c
+++ time_and_memory/gf_generation_formula.c
@@ -2,13 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 struct sieve_t
 {
     int long long n;
     char* s;
 };
-
-int a, b, max_sequence_length;
 
 void construct_sieve(struct sieve_t* sieve)
 {
@@ -42,13 +41,53 @@ void free_sieve(struct sieve_t* sieve)
     sieve->n = 0;
 }
 
-void solve(int N)
+void solve(const int N)
 {
+    int absolute_a = 0, absolute_b = 0, absolute_max_length = 0;
     struct sieve_t sieve;
+
     construct_sieve(&sieve);
     fill_sieve(&sieve);
 
+
+    for (int b = 1; b < N; ++b)
+    {
+        if (sieve.s[b] == 0)
+        {
+            int current_max_length = 0;
+
+            for (int a = -N + 1; a < N; ++a)
+            {
+                int n = 0;
+                while (1)
+                {
+                    const int value = n * n + a * n + b;
+
+                    if (sieve.s[value] == 0)
+                    {
+                        ++current_max_length;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                    ++n;
+                }
+            }
+
+            if (current_max_length > absolute_max_length)
+            {
+                absolute_a = a;
+                absolute_b = b;
+                absolute_max_length = current_max_length;
+            }
+        }
+    }
+
     free_sieve(&sieve);
+
+    printf("%d %d %d", absolute_a, absolute_b, absolute_max_length);
 }
 
 int main()

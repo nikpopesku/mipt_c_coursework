ca585844971e5f16b478693b11ac67b66f584df6
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-10 11:38:29 +0800

    time_and_memory/gf_generation_formula.c

---
+++ time_and_memory/gf_generation_formula.c
@@ -0,0 +1,23 @@
+#include <assert.h>
+#include <stdio.h>
+
+int a, b, max_sequence_length;
+
+void solve(int N)
+{
+
+}
+
+int main()
+{
+    int N = 0;
+
+    const int res = scanf("%d", &N);
+    assert(res == 1);
+
+    solve(N);
+
+    printf("%d %d %d", a, b, max_sequence_length);
+
+    return 0;
+}

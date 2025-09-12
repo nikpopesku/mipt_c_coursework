3b45ab47a8aaa4193f6c5d567867f64cf75b3603
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-12 11:38:23 +0800

    time_and_memory/cc_circular_prime.c

--- time_and_memory/cc_circular_prime.c
+++ time_and_memory/cc_circular_prime.c
@@ -1,16 +1,33 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+struct sieve_t
+{
+    int n;
+    char* s;
+};
+
+void construct_sieve(struct sieve_t* sieve)
+{
+    char* a = calloc(10000000, sizeof(char));
+    sieve->n = 10000000;
+    sieve->s = a;
+}
 
 void solve(int N)
 {
-
 }
 
 int main()
 {
     int N = 0;
+    struct sieve_t sieve;
 
     const int res = scanf("%d", &N);
+
+
+    construct_sieve(&sieve);
     assert(res == 1);
 
     solve(N);

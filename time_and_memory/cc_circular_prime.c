3ab60fb2eb0d794b8d2f46a5e012a9e473c92966
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-12 11:34:38 +0800

    time_and_memory/cc_circular_prime.c

---
+++ time_and_memory/cc_circular_prime.c
@@ -0,0 +1,19 @@
+#include <assert.h>
+#include <stdio.h>
+
+void solve(int N)
+{
+
+}
+
+int main()
+{
+    int N = 0;
+
+    const int res = scanf("%d", &N);
+    assert(res == 1);
+
+    solve(N);
+
+    return 0;
+}

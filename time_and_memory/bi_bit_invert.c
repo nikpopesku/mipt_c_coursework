4272fd34f61c08de2052fd72853c694b0f709b73
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-16 15:51:59 +0800

    time_and_memory/bi_bit_invert.c

---
+++ time_and_memory/bi_bit_invert.c
@@ -0,0 +1,20 @@
+#include <assert.h>
+#include <stdio.h>
+
+
+void solve(int long long N)
+{
+}
+
+
+int main()
+{
+    int long long N = 0;
+
+    const int res = scanf("%lld", &N);
+    assert(res == 1);
+
+    solve(N);
+
+    return 0;
+}

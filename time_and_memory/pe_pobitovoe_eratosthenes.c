c877149be43ba8b9704c53b60f247324c15bbe8c
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-17 12:02:31 +0800

    time_and_memory/pe_pobitovoe_eratosthenes.c

--- time_and_memory/pe_pobitovoe_eratosthenes.c
+++ time_and_memory/pe_pobitovoe_eratosthenes.c
@@ -1,34 +1,5 @@
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "u_template.h"
 
-void construct_sieve(static struct sieve_t* sv)
-{
-    sv->n = 1000000;
-    unsigned char* a = calloc(sv->n, sizeof(unsigned char));
-    sv->s = a;
-}
 
 void fill_sieve(struct sieve_t* sv);
 int is_prime(struct sieve_t* sv, unsigned n);
-
-void solve(int long long N)
-{
-}
-
-
-int main()
-{
-    int long long N = 0;
-
-    const int res = scanf("%lld", &N);
-    assert(res == 1);
-
-    struct sieve_t sieve;
-    construct_sieve(sieve);
-    solve(N);
-
-    return 0;
-}
---
+++ time_and_memory/pe_pobitovoe_eratosthenes_main.c
@@ -0,0 +1,26 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "pe_pobitovoe_eratosthenes.c"
+
+void construct_sieve(static struct sieve_t* sv)
+{
+    sv->n = 1000000;
+    unsigned char* a = calloc(sv->n, sizeof(unsigned char));
+    sv->s = a;
+}
+
+
+int main()
+{
+    int long long N = 0;
+
+    const int res = scanf("%lld", &N);
+    assert(res == 1);
+
+    struct sieve_t sieve;
+    construct_sieve(sieve);
+    fill_sieve(sieve);
+
+    return 0;
+}

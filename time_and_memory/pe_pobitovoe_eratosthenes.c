40e212753d721c4068200dc66b53296c29ab87df
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-17 12:23:16 +0800

    time_and_memory/pe_pobitovoe_eratosthenes.c

--- time_and_memory/pe_pobitovoe_eratosthenes.c
+++ time_and_memory/pe_pobitovoe_eratosthenes.c
@@ -7,17 +7,17 @@ void fill_sieve(const struct sieve_t* sv)
 
     for (int long long i = 2; i * i < sv->n * 8; ++i)
     {
-        if (sv->s[(i - 2) / 8] == 0)
+        if (sv->s[i / 8] & 1 << (i % 8) == 0)
         {
             for (int long long index = i * i; index < sv->n * 8; index += i)
             {
-                sv->s[index] = 1;
+                sv->s[index / 8] = sv->s[index / 8] | 1 << (index % 8);
                 index += i;
             }
         }
     }
 }
+
 int is_prime(struct sieve_t* sv, unsigned n)
 {
-
 }

99f5190557193686443196473b389b5079b1d7fa
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-19 21:30:53 +0800

    time_and_memory/hwe_homework_economy.c

--- time_and_memory/hwe_homework_economy.c
+++ time_and_memory/hwe_homework_economy.c
@@ -41,13 +41,13 @@ int is_prime(const struct sieve_t *sv, const unsigned n) {
     if (n >= 1 && (n - 1) % 6 == 0) {
         const int val = (n - 1) / 6;
 
-        return (sv->mod1[val / 8] & 1 << val % 8) == 0 ? 1 : 0;
+        return (sv->mod1[val / 8] & 1 << (val % 8)) == 0 ? 1 : 0;
     }
 
     if (n >= 5 && (n - 5) % 6 == 0) {
         const int val = (n - 5) / 6;
 
-        return (sv->mod1[val / 8] & 1 << val % 8) == 0 ? 1 : 0;
+        return (sv->mod1[val / 8] & 1 << (val % 8)) == 0 ? 1 : 0;
     }
 
     return 0;

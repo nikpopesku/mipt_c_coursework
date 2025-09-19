94c20c9e86379c188e08233da5f7390c1ef4097e
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-19 19:35:54 +0800

    UN-28983: Refilling boost credits using invoice line item

--- time_and_memory/hwe_homework_economy.c
+++ time_and_memory/hwe_homework_economy.c
@@ -1,31 +1,23 @@
 #include "u_template.h"
 
 
-void fill_sieve(const struct sieve_t* sv)
-{
+void fill_sieve(const struct sieve_t *sv) {
     sv->mod1[0] = 1;
 
-    for (int i = 0; i < sv->n * 8; ++i)
-    {
-        if ((sv->mod1[i / 8] & 1 << i % 8) == 0)
-        {
+    for (int i = 0; i < sv->n * 8; ++i) {
+        if ((sv->mod1[i / 8] & 1 << i % 8) == 0) {
             const int val0 = 6 * i + 1;
-            for (int index = val0 + 6; index < sv->n * 8; index += 6)
-            {
-                if (index % 2 == 0 || index % 3 == 0 || index % 4 == 0 || index % 5 == 0 || index % val0 == 0)
-                {
+            for (int index = val0 + 6; index < sv->n * 8; index += 6) {
+                if (index % 2 == 0 || index % 3 == 0 || index % 4 == 0 || index % 5 == 0 || index % val0 == 0) {
                     const int val = (index - 1) / 6;
                     sv->mod1[val / 8] = sv->mod1[val / 8] | 1 << (val % 8);
                 }
             }
         }
-        if ((sv->mod5[i / 8] & 1 << i % 8) == 0)
-        {
+        if ((sv->mod5[i / 8] & 1 << i % 8) == 0) {
             const int val0 = 6 * i + 5;
-            for (int index = val0 + 6; index < sv->n * 8; index += 6)
-            {
-                if (index % 2 == 0 || index % 3 == 0 || index % 4 == 0 || index % 5 == 0 || index % val0 == 0)
-                {
+            for (int index = val0 + 6; index < sv->n * 8; index += 6) {
+                if (index % 2 == 0 || index % 3 == 0 || index % 4 == 0 || index % 5 == 0 || index % val0 == 0) {
                     const int val = (index - 5) / 6;
                     sv->mod5[val / 8] = sv->mod5[val / 8] | 1 << (val % 8);
                 }
@@ -37,17 +29,14 @@ void fill_sieve(const struct sieve_t* sv)
     assert(sv->mod5[0] == 32);
 }
 
-int is_prime(const struct sieve_t* sv, unsigned n)
-{
-    if (n >= 1 && (n - 1) % 6 == 0)
-    {
+int is_prime(const struct sieve_t *sv, unsigned n) {
+    if (n >= 1 && (n - 1) % 6 == 0) {
         const int val = (n - 1) / 6;
 
         return (sv->mod1[val / 8] & 1 << val % 8) == 0 ? 1 : 0;
     }
 
-    if (n >= 5 && (n - 5) % 6 == 0)
-    {
+    if (n >= 5 && (n - 5) % 6 == 0) {
         const int val = (n - 5) / 6;
 
         return (sv->mod1[val / 8] & 1 << val % 8) == 0 ? 1 : 0;

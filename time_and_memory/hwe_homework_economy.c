e5109a0671f5361a5eb300095a72fbf17f760466
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-19 15:27:16 +0800

    time_and_memory/hwe_homework_economy.c

--- time_and_memory/hwe_homework_economy.c
+++ time_and_memory/hwe_homework_economy.c
@@ -9,7 +9,7 @@ void fill_sieve(const struct sieve_t* sv)
     {
         if ((sv->mod1[i / 8] & 1 << i % 8) == 0)
         {
-            for (int index1 = 6 * i + 1; index1 < sv->n * 8 * 6; index1 += 6)
+            for (int index1 = 6 * i + 1 + 6; index1 < sv->n * 8 * 6; index1 += 6)
             {
                 if (index1 % 2 == 0 || index1 % 3 == 0 || index1 % 4 == 0 || index1 % 5 == 0)
                 {
@@ -19,7 +19,7 @@ void fill_sieve(const struct sieve_t* sv)
         }
         if ((sv->mod5[i / 8] & 1 << i % 8) == 0)
         {
-            for (int index5 = 6 * i + 5; index5 < sv->n * 8 * 6; index5 += 6)
+            for (int index5 = 6 * i + 5 + 6; index5 < sv->n * 8 * 6; index5 += 6)
             {
                 if (index5 % 2 == 0 || index5 % 3 == 0 || index5 % 4 == 0 || index5 % 5 == 0)
                 {

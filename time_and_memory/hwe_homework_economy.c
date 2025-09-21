d9440e1efb2da0d49d853e91ece606ef82b371b3
npopesku <nikolay.popesku@unbiased.co.uk>
2025-09-21 12:53:05 +0800

    time_and_memory/hwe_homework_economy.c

--- time_and_memory/hwe_homework_economy.c
+++ time_and_memory/hwe_homework_economy.c
@@ -6,11 +6,10 @@
 void fill_sieve(const struct sieve_t* sv)
 {
     const long long limit = sv->n * 8;
-    const long long i_limit = (long long)sqrt(limit / 6.0);
 
     sv->mod1[0] = 1;
 
-    for (int i = 0; i < i_limit; ++i)
+    for (int i = 0; 6 * i * i < limit; ++i)
     {
         if ((sv->mod1[i / 8] & 1 << (i % 8)) == 0)
         {

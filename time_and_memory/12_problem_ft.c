568975f5b1f99b37e551fda079971030ad600032
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-15 11:20:00 +0800

    time_and_memory/12_problem_ft.c

--- time_and_memory/12_problem_ft.c
+++ time_and_memory/12_problem_ft.c
@@ -1,14 +1,24 @@
 #include <assert.h>
 #include <stdio.h>
 
-solve(unsigned long long P)
-{
+int gcd(unsigned long long a, unsigned long long b) {
+    if (a == b) {
+        return a;
+    }
 
+    if (a > b) {
+        return gcd(a - b, b)
+    }
+
+    return gcd(b, a);
 }
 
-int main()
-{
-    unsigned long long P= 0;
+int solve(unsigned long long P) {
+    return 4;
+}
+
+int main() {
+    unsigned long long P = 0;
 
     const int res = scanf("%llu", &P);
     assert(res == 1);

9588829dd89b1b08d2f2160b7acb73d8c223ee6d
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-15 11:15:56 +0800

    time_and_memory/12_problem_ft.c

---
+++ time_and_memory/12_problem_ft.c
@@ -0,0 +1,21 @@
+#include <assert.h>
+#include <stdio.h>
+
+solve(unsigned long long P)
+{
+
+}
+
+int main()
+{
+    unsigned long long P= 0;
+
+    const int res = scanf("%llu", &P);
+    assert(res == 1);
+
+    const int answer = solve(P);
+
+    printf("%lld", answer);
+
+    return 0;
+}

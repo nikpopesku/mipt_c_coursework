1e9381f26fcc754a79c5e38de55ff35e419d98b4
Nikolai Popesku <nic1k@yandex.com>
2026-06-12 11:43:40 +0800

    arrays_and_pointers/1_problem_mm.c

--- arrays_and_pointers/1_problem_mm.c
+++ arrays_and_pointers/1_problem_mm.c
@@ -1,4 +1,21 @@
+#include <assert.h>
+#include <stdio.h>
+
 int main()
 {
+    int sz;
+    const int res = scanf("%d", &sz);
+    assert(res == 1);
+
+    if (sz == 0) {
+        printf("0 0");
+
+        return 0;
+    }
+
+    for (int i = 0; i < sz; ++i) {
+
+    }
+
     return 0;
 }

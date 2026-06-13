57099289501ef6e1dcbbeeb32a08f30506a9bfb5
Nikolai Popesku <nic1k@yandex.com>
2026-06-13 13:14:10 +0800

    arrays_and_pointers/3_problem_si.c

--- arrays_and_pointers/3_problem_si.c
+++ arrays_and_pointers/3_problem_si.c
@@ -4,24 +4,24 @@
 int moveright(int *arr, const int key, const int last) {
     for (int i = last - 1; i >= 0; --i) {
         if (arr[i] > key) {
-            arr[i+i] = arr[i];
+            arr[i+1] = arr[i];
         } else {
-            return i;
+            return i + 1;
         }
     }
 
     return 0;
 }
 
-void inssort(int *arr, int len) {
-    int i;
-    for (i = 0; i < len; ++i) {
-        int key, pos;
-        key = arr[i];
-        pos = moveright(arr, key, i);
-        arr[pos] = key;
-    }
-}
+// void inssort(int *arr, int len) {
+//     int i;
+//     for (i = 0; i < len; ++i) {
+//         int key, pos;
+//         key = arr[i];
+//         pos = moveright(arr, key, i);
+//         arr[pos] = key;
+//     }
+// }
 
 int main() {
     int sz;
@@ -32,11 +32,17 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
-    inssort(arr, sz);
+    int last;
+    scanf("%d", &last);
+
+    int pos = moveright(arr, arr[last], last);
 
     for (int i = 0; i < sz; ++i) {
         printf("%d ", arr[i]);
     }
+    printf("\n");
+
+    printf("%d", pos);
 
     free(arr);
 

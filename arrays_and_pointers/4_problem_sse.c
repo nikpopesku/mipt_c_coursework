cd5f674f1dd2922f56a2da78c373c6165216c50f
Nikolai Popesku <nic1k@yandex.com>
2026-06-15 13:14:33 +0800

    arrays_and_pointers/4_problem_sse.c

--- arrays_and_pointers/4_problem_sse.c
+++ arrays_and_pointers/4_problem_sse.c
@@ -19,6 +19,29 @@ int main() {
     res = scanf("%d", &pos);
     assert(res == 1);
 
+    int delta = pos > 0 ? arr[pos] - arr[pos - 1] : arr[0];
+    int new_pos = pos;
+
+    for (int i = pos; i < sz; ++i) {
+        if (pos > 0 && arr[i] >= arr[pos - 1] && delta > arr[i] - arr[pos - 1]) {
+            delta = arr[i] - arr[pos - 1];
+            new_pos = i;
+        } else if (pos == 0 && arr[i] < delta) {
+            delta = arr[i];
+            new_pos = i;
+        }
+    }
+
+    if (pos != new_pos) {
+        const int temp = arr[pos];
+        arr[pos] = arr[new_pos];
+        arr[new_pos] = temp;
+    }
+
+    for (int i = 0; i < sz; ++i) {
+        printf("%d ", arr[i]);
+    }
+
 
     free(arr);
     return 0;

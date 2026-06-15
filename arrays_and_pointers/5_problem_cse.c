7659210a96b6ce9b80dffc261feafcf6d1b9bbf1
Nikolai Popesku <nic1k@yandex.com>
2026-06-15 13:50:36 +0800

    arrays_and_pointers/5_problem_cse.c

--- arrays_and_pointers/5_problem_cse.c
+++ arrays_and_pointers/5_problem_cse.c
@@ -8,12 +8,29 @@ int cmp(void const *lhs, void const *rhs) {
     const int a = *((int const *)lhs);
     const int b = *((int const *)rhs);
 
-    if (a > b) return 1;
+    if (a < b) return 1;
 
     return 0;
 }
 
 int selstep(void *parr, int eltsize, int numelts, int nsorted, cmp_t cmp) {
+    int delta = nsorted > 0 ? parr[nsorted] - parr[nsorted - 1] : parr[0];
+    int new_pos = nsorted;
+
+    for (int i = nsorted; i < numelts; ++i) {
+        if (nsorted > 0 && cmp(parr[nsorted - 1], parr[i]) == 0 && parr[i] - parr[nsorted - 1] < delta) {
+            delta = parr[i] - parr[nsorted - 1];
+        } else if (nsorted == 0 && parr[i] < delta) {
+            delta = parr[i];
+        }
+    }
+
+    if (new_pos != nsorted) {
+        int temp = parr[nsorted];
+        parr[nsorted] = parr[new_pos];
+        parr[new_pos] = temp;
+    }
+
     return 0;
 }
 

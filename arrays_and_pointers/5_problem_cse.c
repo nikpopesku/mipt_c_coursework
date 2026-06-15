f928ce7a65ccdd290a5d8552982d84d1dd18c3c4
Nikolai Popesku <nic1k@yandex.com>
2026-06-15 13:33:56 +0800

    arrays_and_pointers/5_problem_cse.c

--- arrays_and_pointers/5_problem_cse.c
+++ arrays_and_pointers/5_problem_cse.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef int (*cmp_t)(void const * lhs, void const * rhs);
+
+int selstep(void * parr, int eltsize, int numelts, int nsorted, cmp_t cmp) {
+
+    return 0;
+}
+
 int main() {
     int sz;
     int res = scanf("%d", &sz);
@@ -15,6 +22,10 @@ int main() {
     int pos;
     res = scanf("%d", &pos);
     assert(res == 1);
+
+    for (int i = 0; i < sz; ++i) {
+        printf("%d ", arr[i]);
+    }
 
     free(arr);
 

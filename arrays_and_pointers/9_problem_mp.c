de11c686069da567c0d0651106fad417e859c4ad
Nikolai Popesku <nic1k@yandex.com>
2026-06-23 12:14:12 +0800

    arrays_and_pointers/9_problem_mp.c

--- arrays_and_pointers/9_problem_mp.c
+++ arrays_and_pointers/9_problem_mp.c
@@ -4,6 +4,30 @@
 
 
 int main() {
+    int sz1;
+    int sz2;
+    int i;
+    int * arr1;
+    int * arr2;
+    int res = scanf("%d", &sz1);
+    assert(res == 1);
+    res = scanf("%d", &sz2);
+    assert(res == 2);
+    arr1 = calloc(sz1, sizeof(int));
+    arr2 = calloc(sz2, sizeof(int));
+
+    for (i = 0; i < sz1; ++i) {
+        res = scanf("%d", &arr1[i]);
+        assert(res == 1);
+    }
+
+    for (i = 0; i < sz2; ++i) {
+        res = scanf("%d", &arr2[i]);
+        assert(res == 1);
+    }
+
+    free(arr1);
+    free(arr2);
 
     return 0;
 }

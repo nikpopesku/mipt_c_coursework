95f9d618bc545d201ad8f427f1033c0f78f79b52
Nikolai Popesku <nic1k@yandex.com>
2026-06-27 12:57:55 +0800

    arrays_and_pointers/14_problem_rs.c

--- arrays_and_pointers/14_problem_rs.c
+++ arrays_and_pointers/14_problem_rs.c
@@ -4,9 +4,12 @@
 
 int main() {
     unsigned sz;
-    int * arr;
+    int *arr;
     int i;
+    int radix;
+    int base = 1;
     int res = scanf("%u", &sz);
+    int *bucket = calloc(10, sizeof(int));
     assert(res == 1);
 
     arr = calloc(sz, sizeof(int));
@@ -15,6 +18,12 @@ int main() {
         res = scanf("%d", &arr[i]);
         assert(res == 1);
     }
+
+    res = scanf("%d", &radix);
+    assert(res == 1);
+
+    for (i = 0; i < radix; ++i) base *= 10;
+    for (i = 0; i < sz; ++i) ++bucket[(arr[i] / base) % 10];
 
     return 0;
 }

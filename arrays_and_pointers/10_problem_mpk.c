1348bcd23b06c17c95bf5c13713e1ad9ec81b2f4
Nikolai Popesku <nic1k@yandex.com>
2026-06-30 12:19:01 +0800

    arrays_and_pointers/10_problem_mpk.c

--- arrays_and_pointers/10_problem_mpk.c
+++ arrays_and_pointers/10_problem_mpk.c
@@ -1,14 +1,16 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void karatsuba(int *arr1, int *arr2, int *res, unsigned n) {
     int i;
     int j;
     int k = n / 2;
-    int l[k] = {0};
-    int r[k] = {0};
-    int t[n] = {0};
+    int l[k];
+    int r[k];
+    int t[n];
+    memset(t, 0, n * sizeof(int));
     int *t1 = t, *t2 = t + k;
     int *s1 = res, *s2 = res + k, *s3 = res + 2 * k, *s4 = res + 3 * k;
 
@@ -71,6 +73,10 @@ int main() {
     }
 
     printf("\n");
+
+    free(arr1);
+    free(arr2);
+    free(result);
 
     return 0;
 }

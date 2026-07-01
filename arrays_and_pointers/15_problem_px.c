31d14e074f09a0a77bc39e4a6e3e0ac54a9f33e7
Nikolai Popesku <nic1k@yandex.com>
2026-07-01 13:38:58 +0800

    arrays_and_pointers/15_problem_px.c

---
+++ arrays_and_pointers/15_problem_px.c
@@ -0,0 +1,18 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void powNxN (unsigned (*A)[N], unsigned k, unsigned m) {
+
+}
+
+int main() {
+    int k, m;
+    int res = scanf("%d", &k);
+    assert(res == 1);
+    res = scanf("%d", &m);
+    assert(res == 1);
+
+
+    return 0;
+}

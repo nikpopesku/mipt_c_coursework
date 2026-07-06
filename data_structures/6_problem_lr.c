679461c96ee8fa19811974614ba8e9a8641c9859
Nikolai Popesku <nic1k@yandex.com>
2026-07-06 13:50:46 +0800

    data_structures/6_problem_lr.c

--- data_structures/6_problem_lr.c
+++ data_structures/6_problem_lr.c
@@ -19,8 +19,19 @@ struct node_t {
 // }
 
 
-struct node_t * reverse(struct node_t *top) {
+struct node_t *reverse(struct node_t *top) {
+    if (top == NULL) return NULL;
+    if (top->next == NULL) return top;
+    struct node_t *prev = top;
+    struct node_t *cur = top->next;
+    prev->next = NULL;
 
+    while (cur != NULL) {
+        struct node_t *nxt = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = nxt;
+    }
 }
 
 int main() {

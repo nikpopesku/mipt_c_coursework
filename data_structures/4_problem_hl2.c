a7220160a73534d47129aefc958d7a94ec5b36a8
Nikolai Popesku <nic1k@yandex.com>
2026-07-06 12:04:18 +0800

    data_structures/4_problem_hl2.c

--- data_structures/4_problem_hl2.c
+++ data_structures/4_problem_hl2.c
@@ -1,5 +1,4 @@
 #include <assert.h>
-#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,31 +7,42 @@ struct node_t {
     int data;
 };
 
-int list_is_a_loop(struct node_t *top) {
-    if (top == NULL) return 0;
-
-    const struct node_t *turtle = top;
-    const struct node_t *rabbit = top;
+struct node_t * list_is_a_loop(struct node_t *turtle, struct node_t * rabbit) {
+    if (rabbit == NULL) return 0;
 
     do {
         rabbit = rabbit->next;
 
-        if (rabbit == turtle) return 1;
+        if (rabbit == turtle) return rabbit;
 
         if (rabbit == NULL) return 0;
 
         rabbit = rabbit->next;
         turtle = turtle->next;
 
-        if (rabbit == turtle) return 1;
+        if (rabbit == turtle) return rabbit;
     } while (rabbit && turtle);
 
-    return 0;
+    return NULL;
 }
 
 // определяет длину петли в односвязном списке
 // возвращает длину если есть, 0 если петли нет
-int loop_len(struct node_t *top);
+int loop_len(struct node_t *top) {
+    struct node_t * rabbit = list_is_a_loop(top, top);
+    if (rabbit == NULL) return 0;
+
+    struct node_t * turtle = top;
+    int counter = 0;
+
+    while (turtle != rabbit) {
+        ++counter;
+        rabbit = rabbit->next;
+        turtle = turtle->next;
+    }
+
+    return counter;
+}
 
 int main() {
     return 0;

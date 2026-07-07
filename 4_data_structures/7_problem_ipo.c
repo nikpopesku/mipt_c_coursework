68cc3ed5b657c2f2fc535e93545930ea3348a4c5
Nikolai Popesku <nic1k@yandex.com>
2026-07-07 11:57:02 +0800

    4_data_structures/7_problem_ipo.c

---
+++ 4_data_structures/7_problem_ipo.c
@@ -0,0 +1,28 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct tree_t {
+    struct tree_t *left;
+    struct tree_t *right;
+    int data;
+};
+
+void print_pre(struct tree_t *top) {
+    if (top == NULL) return;
+
+    printf("%d ", top->data);
+
+    if (top->left) {
+        print_pre(top->left);
+    }
+
+    if (top->right) {
+        print_pre(top->right);
+    }
+}
+
+
+int main() {
+    return 0;
+}

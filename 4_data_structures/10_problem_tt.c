276dec115b672fc1c24b093d4ed819f3e5422ec4
Nikolai Popesku <nic1k@yandex.com>
2026-07-09 12:15:30 +0800

    4_data_structures/10_problem_tt.c

--- 4_data_structures/10_problem_tt.c
+++ 4_data_structures/10_problem_tt.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_postorder(int *preorder, int *inorder, const int begin_inorder, const int end_inorder) {
+void print_postorder(int *preorder, int *inorder, const int begin_preorder, const int begin_inorder, const int end_inorder) {
     int count = 0;
     printf("%d ", preorder[0]);
 
     if (begin_inorder == end_inorder) return;
 
     for (count = begin_inorder; count < end_inorder; ++count) {
-        if (inorder[count] == preorder[0]) break;
+        if (inorder[count] == preorder[begin_preorder]) break;
     }
 
-    print_postorder(preorder, inorder, begin_inorder, count - 1);
+    print_postorder(preorder, inorder, begin_preorder + 1, begin_inorder, count - 1);
 }
 
 
@@ -37,7 +37,7 @@ int main() {
         assert(res == 1);
     }
 
-    print_postorder(preorder, inorder, 0, sz - 1);
+    print_postorder(preorder, inorder, 0, 0, sz - 1);
 
     free(preorder);
     free(inorder);

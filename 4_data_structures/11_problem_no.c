d94cf6ae9d74e308bfe002996b5d8ed65b5b5444
Nikolai Popesku <nic1k@yandex.com>
2026-07-10 12:06:36 +0800

    4_data_structures/11_problem_no.c

--- 4_data_structures/11_problem_no.c
+++ 4_data_structures/11_problem_no.c
@@ -4,12 +4,17 @@
 
 
 int main() {
-    FILE fptr = fopen("input.txt", "r");
+    FILE *fptr = fopen("input.txt", "r");
+    unsigned sz;
+    int res;
 
     if (fptr == NULL) { // 3. Check for errors
         printf("Error opening file!\n");
         return 1;
     }
+
+    res = fscanf(fptr, "%u", &sz);
+    assert(res == 1);
 
     fclose(fptr);
 

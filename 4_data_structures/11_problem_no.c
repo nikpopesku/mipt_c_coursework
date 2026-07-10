5c499c08920d9756414ba1c157bc724e8cf112b2
Nikolai Popesku <nic1k@yandex.com>
2026-07-10 12:08:18 +0800

    4_data_structures/11_problem_no.c

--- 4_data_structures/11_problem_no.c
+++ 4_data_structures/11_problem_no.c
@@ -7,6 +7,8 @@ int main() {
     FILE *fptr = fopen("input.txt", "r");
     unsigned sz;
     int res;
+    int *arr;
+    int i;
 
     if (fptr == NULL) { // 3. Check for errors
         printf("Error opening file!\n");
@@ -15,6 +17,13 @@ int main() {
 
     res = fscanf(fptr, "%u", &sz);
     assert(res == 1);
+
+    arr = malloc(sz * sizeof(int));
+    for (i = 0; i < sz; ++i) {
+        res = fscanf(fptr, "%d", &arr[i]);
+        assert(res == 1);
+    }
+
 
     fclose(fptr);
 

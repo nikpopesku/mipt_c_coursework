34d5b52a65b602c6b104f8d1b03002942442ade4
Nikolai Popesku <nic1k@yandex.com>
2026-07-10 12:46:03 +0800

    4_data_structures/12_problem_xc.c

--- 4_data_structures/12_problem_xc.c
+++ 4_data_structures/12_problem_xc.c
@@ -3,13 +3,14 @@
 #include <stdlib.h>
 
 int HASH_MAX = 100;
+
 typedef int (*get_hash_t)(const char *s);
 
 int ncollisions(char **strs, int n, get_hash_t f) {
     int counter = 0;
     int i = 0;
     int val;
-    int * arr = calloc(HASH_MAX, sizeof(int));
+    int *arr = calloc(HASH_MAX, sizeof(int));
 
     for (i = 0; i < n; ++i) {
         val = f(strs[i]);

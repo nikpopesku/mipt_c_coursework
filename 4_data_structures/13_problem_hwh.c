795326f2210e63735dbd860a999a6d41141c4aea
Nikolai Popesku <nic1k@yandex.com>
2026-07-13 14:40:39 +0800

    4_data_structures/13_problem_hwh.c

--- 4_data_structures/13_problem_hwh.c
+++ 4_data_structures/13_problem_hwh.c
@@ -4,9 +4,26 @@
 
 
 int main() {
-    unsigned sz;
-    int res = scanf("%u", &sz);
+    unsigned answer;
+    unsigned sz1;
+    unsigned sz2;
+    unsigned i = 0;
+    char word[100];
+    char ch;
+    // int * arr = calloc(26, siz)
+    int res = scanf("%u", &answer);
     assert(res == 1);
+
+    res = scanf("%u", &sz1);
+    assert(res == 1);
+
+
+    while (i < sz1) {
+        ch = getchar();
+        if (ch == ' ' || ch == '\n' || ch == EOF) {
+
+        }
+    }
 
 
 

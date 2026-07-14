f3b61f504ac9c30bbbdcb280238eb1d2af7db99e
Nikolai Popesku <nic1k@yandex.com>
2026-07-14 11:42:14 +0800

    4_data_structures/13_problem_hwh.c

--- 4_data_structures/13_problem_hwh.c
+++ 4_data_structures/13_problem_hwh.c
@@ -2,29 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct node_t {
+    struct node_t *next;
+    char* data;
+};
+
 
 int main() {
     unsigned answer;
     unsigned sz1;
     unsigned sz2;
-    unsigned i = 0;
-    char word[100];
-    char ch;
-    // int * arr = calloc(26, siz)
+    char* word = NULL;
+    struct node_t *bucket = calloc(26, sizeof(struct node_t));
     int res = scanf("%u", &answer);
     assert(res == 1);
 
     res = scanf("%u", &sz1);
     assert(res == 1);
 
-
-    while (i < sz1) {
-        ch = getchar();
-        if (ch == ' ' || ch == '\n' || ch == EOF) {
-
-        }
+    while (scanf("%ms", &word) == 1) {
+        struct node_t* node = calloc(1, sizeof(struct node_t));
+        node->data = word;
+        free(word);
     }
-
 
 
     return 0;

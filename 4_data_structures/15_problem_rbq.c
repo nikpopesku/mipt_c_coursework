909a674e7d4bcc81e35d4dd692503c85378225f3
Nikolai Popesku <nic1k@yandex.com>
2026-07-16 12:10:04 +0800

    4_data_structures/15_problem_rbq.c

--- 4_data_structures/15_problem_rbq.c
+++ 4_data_structures/15_problem_rbq.c
@@ -1,6 +1,21 @@
-#include <string.h>
-#include "func.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+    unsigned sz;
+    unsigned i;
+    unsigned * distance;
+    int res = scanf("%u", &sz);
+    assert(res == 1);
+    distance = calloc(sz, sizeof(unsigned));
+
+    for (i = 0; i < sz; ++i) {
+        res = scanf("%u", &distance[i]);
+    }
+
+
+    free(distance);
+
     return 0;
 }

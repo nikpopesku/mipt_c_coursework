d5c563e6cda7dbd4595eb1b3a12c2e40ed75ad88
Nikolai Popesku <nic1k@yandex.com>
2026-07-16 12:03:10 +0800

    4_data_structures/14_problem_kc.c

--- 4_data_structures/14_problem_kc.c
+++ 4_data_structures/14_problem_kc.c
@@ -15,19 +15,19 @@ unsigned long long pow_mod (unsigned n, unsigned k, unsigned m);
 // возвращает номер позиции на которой needle расположена внутри haystack
 // или -1 если ничего не найдено
 int rabin_karp(const char *needle, const char *haystack) {
-    unsigned n, target, cur, count = 0, left = 0, right = strlen(needle);
+    unsigned n, target, cur, left = 0, right = strlen(needle);
 
     target = get_hash(needle, needle + right);
     cur = get_hash(haystack, haystack + right);
     n = pow_mod(R, right - 1, Q);
 
-    while(target != cur && haystack[right] != 0 && strncmp(haystack + left, needle, right - left) != 0) {
+    while(haystack[right] != 0 && (target != cur || strncmp(haystack + left, needle, right - left) != 0)) {
         cur = update_hash(cur, n, haystack[left], haystack[right]);
         left += 1;
         right += 1;
     }
 
-    return (target == cur) ? left : -1;
+    return (target == cur) && strncmp(haystack + left, needle, right - left) == 0 ? left : -1;
 }
 
 int main() {

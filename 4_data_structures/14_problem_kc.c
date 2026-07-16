005cc3aacb2867c8a37166820b2a37d0206ee3b4
Nikolai Popesku <nic1k@yandex.com>
2026-07-16 11:54:20 +0800

    4_data_structures/14_problem_kc.c

--- 4_data_structures/14_problem_kc.c
+++ 4_data_structures/14_problem_kc.c
@@ -1,7 +1,5 @@
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include "func.h"
 
 // подсчитать хеш строки от start до fin
 int get_hash(const char *start, const char *fin);

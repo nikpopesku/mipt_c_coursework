08e67a46ec9870b4462a41d535a849e67d0d654d
Nikolai Popesku <nic1k@yandex.com>
2026-07-16 12:12:46 +0800

    4_data_structures/15_problem_rbq.c

--- 4_data_structures/15_problem_rbq.c
+++ 4_data_structures/15_problem_rbq.c
@@ -3,9 +3,7 @@
 #include <stdlib.h>
 
 int main() {
-    unsigned sz;
-    unsigned q;
-    unsigned i;
+    unsigned sz, q, i, left, right;
     unsigned * distance;
     int res = scanf("%u", &sz);
     assert(res == 1);
@@ -13,10 +11,17 @@ int main() {
 
     for (i = 0; i < sz; ++i) {
         res = scanf("%u", &distance[i]);
+        assert(res == 1);
     }
 
     res = scanf("%u", &q);
     assert(res == 1);
+    for (i = 0; i < q; ++i) {
+        res = scanf("%u", &left);
+        assert(res == 1);
+        res = scanf("%u", &right);
+        assert(res == 1);
+    }
 
     free(distance);
 

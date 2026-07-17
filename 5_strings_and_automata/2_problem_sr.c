c9e0f710d72121a76c864080d778e7ae4a73011b
Nikolai Popesku <nic1k@yandex.com>
2026-07-17 12:12:21 +0800

    5_strings_and_automata/2_problem_sr.c

--- 5_strings_and_automata/2_problem_sr.c
+++ 5_strings_and_automata/2_problem_sr.c
@@ -1,5 +1,27 @@
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+    unsigned sz1, sz2;
+    unsigned i;
+    char * word;
+    char * sentence;
+    int res = scanf("%d", &sz1);
+    assert(res == 1);
+
+    word = calloc(sz1 + 1, sizeof(char));
+    for (i = 0; i < sz1; ++i) {
+        word[i] = getchar();
+    }
+
+    res = scanf("%d", &sz2);
+    assert(res == 1);
+
+    sentence = calloc(sz2 + 1, sizeof(char));
+    for (i = 0; i < sz2; ++i) {
+        sentence[i] = getchar();
+    }
+
     return 0;
 }

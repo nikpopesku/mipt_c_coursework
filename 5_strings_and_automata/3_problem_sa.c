3ec407c93e6bd73ae86fabf8ebc10d3b53ed98f5
Nikolai Popesku <nic1k@yandex.com>
2026-07-19 12:18:24 +0800

    5_strings_and_automata/3_problem_sa.c

--- 5_strings_and_automata/3_problem_sa.c
+++ 5_strings_and_automata/3_problem_sa.c
@@ -4,11 +4,18 @@
 #include <string.h>
 
 char *strcat_r(char *dest, const char *src, int *bufsz) {
-    int i, j;
-    for (i = 0; i < *bufsz && dest[i] != '\0'; ++i) {}
-    for (j = 0; src[i] != '\0'; ++j) {}
-    if (i + j > *bufsz) {
-        realloc(dest, i + j);
+    int i, j, k;
+    for (i = 0; i < *bufsz && dest[i] != '\0'; ++i) {
+    }
+    for (j = 0; src[i] != '\0'; ++j) {
+    }
+    if (i - 1 + j > *bufsz) {
+        realloc(dest, i + j - 1);
+        *bufsz = i + j - 1;
+    }
+
+    for (k = 0; k < j; ++k) {
+        dest[k + i - 1] = src[k];
     }
 }
 

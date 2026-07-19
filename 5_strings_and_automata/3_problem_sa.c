1162d5776272d70cd9a1381eb8e31a5e59e9ce59
Nikolai Popesku <nic1k@yandex.com>
2026-07-19 12:42:32 +0800

    5_strings_and_automata/3_problem_sa.c

--- 5_strings_and_automata/3_problem_sa.c
+++ 5_strings_and_automata/3_problem_sa.c
@@ -7,25 +7,55 @@ char *strcat_r(char *dest, const char *src, int *bufsz) {
     int i, j, k;
     for (i = 0; i < *bufsz && dest[i] != '\0'; ++i) {
     }
-    for (j = 0; src[i] != '\0'; ++j) {
+    for (j = 0; src[j] != '\0'; ++j) {
     }
-    if (i - 1 + j > *bufsz) {
-        realloc(dest, i + j - 1);
-        *bufsz = i + j - 1;
+    if (i + j + i > *bufsz) {
+        dest = realloc(dest, i + j + 1);
+        *bufsz = i + j + 1;
     }
 
     for (k = 0; k < j; ++k) {
-        dest[k + i - 1] = src[k];
+        dest[k + i] = src[k];
     }
+    dest[k + i] = '\0';
 
     return dest;
 }
 
 
 int main() {
-    unsigned sz;
-    int res = scanf("%u", &sz);
+    unsigned i = 0;
+    char *src, *dest;
+    char ch;
+    int sz, *ptr_sz;
+
+    int res = scanf("%d", &sz);
     assert(res == 1);
+
+    getchar();
+
+    dest = calloc(sz, sizeof(char));
+
+    while ((ch = getchar()) != '\n') {
+        dest[i++] = ch;
+    }
+    dest[i] = '\0';
+
+    i = 0;
+    src = calloc(8, sizeof(char));
+
+    while ((ch = getchar()) != '\n') {
+        src[i++] = ch;
+    }
+    src[i] = '\0';
+
+
+    ptr_sz = &sz;
+    strcat_r(dest, src, ptr_sz);
+
+    for (i = 0; i < *ptr_sz; ++i) {
+        printf("%c", dest[i]);
+    }
 
 
     return 0;

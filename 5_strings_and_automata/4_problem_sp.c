f3614b1348dcb30ee1e4f312e7c1e5190c3ab097
Nikolai Popesku <nic1k@yandex.com>
2026-07-19 13:31:03 +0800

    5_strings_and_automata/4_problem_sp.c

--- 5_strings_and_automata/4_problem_sp.c
+++ 5_strings_and_automata/4_problem_sp.c
@@ -1,9 +1,21 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char *replace(char *str, char const *from, char const *to) {
+    char *p;
+    char *source = str;
+    int i;
 
+
+    while ((p = strstr(source, from)) != NULL) {
+        for (i = 0; to[i] != '\0'; ++i) {
+            p[i] = to[i];
+        }
+
+        source = p + i;
+    }
 }
 
 int main() {

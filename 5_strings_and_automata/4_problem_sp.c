70f3cde9bb3404796d81d3bf4db60af21cc3578b
Nikolai Popesku <nic1k@yandex.com>
2026-07-19 13:35:52 +0800

    5_strings_and_automata/4_problem_sp.c

--- 5_strings_and_automata/4_problem_sp.c
+++ 5_strings_and_automata/4_problem_sp.c
@@ -20,9 +20,9 @@ char *replace(char *str, char const *from, char const *to) {
 }
 
 int main() {
-    unsigned sz1, i = 0;
+    unsigned sz1, sz2, i = 0;
     char ch;
-    char * from;
+    char *from, *to;
     int res = scanf("%u", &sz1);
     assert(res == 1);
 
@@ -33,6 +33,16 @@ int main() {
     }
     from[i] = '\0';
 
+    res = scanf("%u", &sz2);
+    assert(res == 1);
+
+    i = 0;
+    to = calloc(sz2, sizeof(char));
+
+    while ((ch = getchar()) != '\n') {
+        to[i++] = ch;
+    }
+    to[i] = '\0';
 
     return 0;
 }

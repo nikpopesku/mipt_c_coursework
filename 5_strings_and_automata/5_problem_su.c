41011dcfc1f25d4fb2746b6477b796d6738ca990
Nikolai Popesku <nic1k@yandex.com>
2026-07-20 12:54:18 +0800

    5_strings_and_automata/5_problem_su.c

--- 5_strings_and_automata/5_problem_su.c
+++ 5_strings_and_automata/5_problem_su.c
@@ -32,30 +32,32 @@ int main() {
 
     getchar();
 
-    haystack = calloc(sz1, sizeof(char));
+    haystack = calloc(sz1 + 1, sizeof(char));
 
     for (i = 0; i < sz1; ++i) {
         res = scanf("%c", &haystack[i]);
         assert(res == 1);
     }
+    haystack[i] = '\0';
 
     res = scanf("%u", &sz2);
     assert(res == 1);
 
     getchar();
 
-    needle = calloc(sz2, sizeof(char));
+    needle = calloc(sz2 + 1, sizeof(char));
 
     for (i = 0; i < sz2; ++i) {
         res = scanf("%c", &needle[i]);
         assert(res == 1);
     }
+    needle[i] = '\0';
 
     new_haystack = strstrci(needle, haystack);
 
 
     for (i = 0; i < strlen(new_haystack); ++i) {
-        printf("%c", haystack[i]);
+        printf("%c", new_haystack[i]);
     }
 
     free(haystack);

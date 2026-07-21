622a32b82e256e211bbdac6754cff6af3b448a31
Nikolai Popesku <nic1k@yandex.com>
2026-07-21 11:52:39 +0800

    5_strings_and_automata/5_problem_rxa.c

---
+++ 5_strings_and_automata/5_problem_rxa.c
@@ -0,0 +1,81 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+
+int main() {
+    char * word;
+    int i = 0;
+    int position = 1;
+
+    while ((ch = getchar()) != EOF) {
+        switch (ch) {
+            case 'a':
+                switch (position) {
+                    case 1:
+                        position = 1;
+                        break;
+                    case 2:
+                        position = 2;
+                        break;
+                    case 3:
+                        position = 5;
+                        break;
+                    case 4:
+                        position = 5;
+                        break;
+                    case 5:
+                        position = 5;
+                        break;
+                }
+                break;
+            case 'b':
+                switch (position) {
+                    case 1:
+                        position = 2;
+                        break;
+                    case 2:
+                        position = 2;
+                        break;
+                    case 3:
+                        position = 4;
+                        break;
+                    case 4:
+                        position = 4;
+                        break;
+                    case 5:
+                        position = 5;
+                        break;
+                }
+                break;
+            case 'c':
+                switch (position) {
+                    case 1:
+                        position = 5;
+                        break;
+                    case 2:
+                        position = 3;
+                        break;
+                    case 3:
+                        position = 3;
+                        break;
+                    case 4:
+                        position = 3;
+                        break;
+                    case 5:
+                        position = 5;
+                        break;
+                }
+                break;
+            default:
+                printf("%d", position == 2 || position == 3 || position == 4);
+                return 0;
+        }
+    }
+
+    printf("%d", position == 2 || position == 3 || position == 4);
+
+    return 0;
+}

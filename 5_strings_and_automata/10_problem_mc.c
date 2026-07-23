5683710bce85ff47e1b00879f50b236fe31b39d4
Nikolai Popesku <nic1k@yandex.com>
2026-07-23 11:54:42 +0800

    5_strings_and_automata/10_problem_mc.c

--- 5_strings_and_automata/10_problem_mc.c
+++ 5_strings_and_automata/10_problem_mc.c
@@ -4,20 +4,32 @@
 #include <regex.h>
 
 int main() {
-    unsigned amount, sz, i;
+    unsigned amount, sz, c, i;
     int res = scanf("%u", &amount);
-    unsigned *coin;
+    unsigned *coin, *dp;
     assert(res == 1);
 
     res = scanf("%u", &sz);
     assert(res == 1);
 
     coin = calloc(sz, sizeof(unsigned));
+    dp = calloc(amount + 1, sizeof(unsigned));
 
     for (i = 0; i < sz; ++i) {
         res = scanf("%u", &coin[i]);
+        dp[coin[i]] = 1;
         assert(res == 1);
     }
+
+    for (i = 1; i <= amount; ++i) {
+        for (c = 0; c < sz; ++c) {
+            if (i >= coin[c]) {
+                dp[i] = dp[i - coin[c]] + 1 > dp[i] ? dp[i] : dp[i - coin[c]] + 1;
+            }
+        }
+    }
+
+    printf("%u", dp[amount]);
 
 
     free(coin);

7ba37eecd4d2799b0c1aeb39b5f9a228dc804410
Nikolai Popesku <nic1k@yandex.com>
2026-07-24 12:14:49 +0800

    5_strings_and_automata/11_problem_bp.c

--- 5_strings_and_automata/11_problem_bp.c
+++ 5_strings_and_automata/11_problem_bp.c
@@ -4,7 +4,7 @@
 
 int main() {
     unsigned backpack, sz;
-    unsigned *weight, i;
+    unsigned *weight, row, col;
     int **dp;
     int res = scanf("%u", &backpack);
     assert(res == 1);
@@ -12,16 +12,22 @@ int main() {
     res = scanf("%u", &sz);
     assert(res == 1);
 
-    weight = calloc(sz, sizeof(unsigned));
+    weight = calloc(sz + 1, sizeof(unsigned));
 
-    for (int i = 0; i < sz; ++i) {
+    for (int i = 1; i <= sz; ++i) {
         scanf("%u", &weight[i]);
     }
 
     dp = calloc(sz + 1, sizeof(int *));
 
-    for (i = 0; i <= sz; ++i) {
-        dp[i] = calloc(backpack + 1, sizeof(int));
+    for (row = 0; row <= sz; ++row) {
+        dp[row] = calloc(backpack + 1, sizeof(int));
+    }
+
+    for (row = 1; row <= sz; ++row) {
+        for (col = 1; col <= backpack; ++col) {
+            dp[row][col] = max(dp[row])
+        }
     }
 
     return 0;

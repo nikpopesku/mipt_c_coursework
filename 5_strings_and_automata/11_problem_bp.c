f7a5d17f149ec068b6af3522ac99377bda7c6c6e
Nikolai Popesku <nic1k@yandex.com>
2026-07-24 12:08:58 +0800

    5_strings_and_automata/11_problem_bp.c

--- 5_strings_and_automata/11_problem_bp.c
+++ 5_strings_and_automata/11_problem_bp.c
@@ -4,7 +4,8 @@
 
 int main() {
     unsigned backpack, sz;
-    unsigned * weight;
+    unsigned *weight, i;
+    int **dp;
     int res = scanf("%u", &backpack);
     assert(res == 1);
 
@@ -15,6 +16,12 @@ int main() {
 
     for (int i = 0; i < sz; ++i) {
         scanf("%u", &weight[i]);
+    }
+
+    dp = calloc(sz + 1, sizeof(int *));
+
+    for (i = 0; i <= sz; ++i) {
+        dp[i] = calloc(backpack + 1, sizeof(int));
     }
 
     return 0;

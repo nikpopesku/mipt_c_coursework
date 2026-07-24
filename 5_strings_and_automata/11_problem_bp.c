18e387a808648bf4139e8f466e7a436d7c0c74ae
Nikolai Popesku <nic1k@yandex.com>
2026-07-24 12:39:11 +0800

    5_strings_and_automata/11_problem_bp.c - added struct

--- 5_strings_and_automata/11_problem_bp.c
+++ 5_strings_and_automata/11_problem_bp.c
@@ -6,10 +6,15 @@ unsigned max(const unsigned a, const unsigned b) {
     return a > b ? a : b;
 }
 
+struct val {
+    unsigned weight;
+    unsigned count;
+};
+
 int main() {
     unsigned backpack, sz;
     unsigned *weight, row, col;
-    unsigned **dp;
+    struct val **dp;
     int res = scanf("%u", &backpack);
     assert(res == 1);
 
@@ -22,19 +27,19 @@ int main() {
         scanf("%u", &weight[i]);
     }
 
-    dp = calloc(sz + 1, sizeof(unsigned *));
+    dp = calloc(sz + 1, sizeof(struct val *));
 
     for (row = 0; row <= sz; ++row) {
-        dp[row] = calloc(backpack + 1, sizeof(unsigned));
+        dp[row] = calloc(backpack + 1, sizeof(struct val));
     }
 
     for (row = 1; row <= sz; ++row) {
         for (col = 1; col <= backpack; ++col) {
-            dp[row][col] = max(dp[row - 1][col], col >= weight[row] ? dp[row - 1][col - weight[row]] + weight[row] : 0);
+            dp[row][col].weight = max(dp[row - 1][col].weight, col >= weight[row] ? dp[row - 1][col - weight[row]].weight + weight[row] : 0);
         }
     }
 
-    printf("%u\n", dp[sz][backpack]);
+    printf("%u\n", dp[sz][backpack].count);
 
     for (row = 1; row <= sz; ++row) {
         free(dp[row]);

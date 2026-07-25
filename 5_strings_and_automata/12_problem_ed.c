e63f409baf9c5d2e4735928bdf3dc3bcffea0867
Nikolai Popesku <nic1k@yandex.com>
2026-07-25 12:15:04 +0800

    5_strings_and_automata/12_problem_ed.c

---
+++ 5_strings_and_automata/12_problem_ed.c
@@ -0,0 +1,45 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+    unsigned add, delete, edit;
+    unsigned sz1, sz2, i;
+    char * st1, *st2;
+
+    int res = scanf("%u", &add);
+    assert(res == 1);
+
+    res = scanf("%u", &delete);
+    assert(res == 1);
+
+    res = scanf("%u", &edit);
+    assert(res == 1);
+
+    res = scanf("%u", &sz1);
+    assert(res == 1);
+
+    st1 = calloc(sz1 + 1, sizeof(char));
+    for (i = 0; i < sz1; ++i) {
+        res = scanf("%c", &st1[i]);
+        assert(res == 1);
+    }
+
+    st1[i] = '\0';
+
+    res = scanf("%u", &sz2);
+    assert(res == 1);
+
+    st2 = calloc(sz2 + 1, sizeof(char));
+    for (i = 0; i < sz2; ++i) {
+        res = scanf("%c", &st2[i]);
+        assert(res == 1);
+    }
+
+    st2[i] = '\0';
+
+
+    return 0;
+}

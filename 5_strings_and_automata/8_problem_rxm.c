78bb519eb0c35159bce555852b219b8a29b08931
Nikolai Popesku <nic1k@yandex.com>
2026-07-26 12:10:35 +0800

    5_strings_and_automata/8_problem_rxm.c

--- 5_strings_and_automata/8_problem_rxm.c
+++ 5_strings_and_automata/8_problem_rxm.c
@@ -7,17 +7,20 @@ int main() {
     unsigned sz1, sz2, i;
     char *needle, *haystack;
     regex_t re;
+    regmatch_t match;
     int res = scanf("%u", &sz1);
     assert(res == 1);
 
     getchar();
 
-    needle = calloc(sz1, sizeof(char));
+    needle = calloc(sz1 + 1, sizeof(char));
 
     for (i = 0; i < sz1; ++i) {
         res = scanf("%c", &needle[i]);
         assert(res == 1);
     }
+
+    needle[i] = '\0';
 
     getchar();
 
@@ -26,17 +29,21 @@ int main() {
 
     getchar();
 
-    haystack = calloc(sz2, sizeof(char));
+    haystack = calloc(sz2 + 1, sizeof(char));
 
     for (i = 0; i < sz2; ++i) {
         res = scanf("%c", &haystack[i]);
         assert(res == 1);
     }
 
+    haystack[i] = '\0';
 
     res = regcomp(&re, needle, REG_EXTENDED);
     assert(res == 0);
 
+    while (regexec(&re, haystack, 1, &match, 0) != 0) {
+        res = 2;
+    }
 
     return 0;
 }

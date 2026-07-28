0f1b145820167ee46f3b24aa06fddee0c8482c75
Nikolai Popesku <nic1k@yandex.com>
2026-07-28 12:31:23 +0800

    5_strings_and_automata/9_problem_hwk.c.

--- 5_strings_and_automata/9_problem_hwk.c
+++ 5_strings_and_automata/9_problem_hwk.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "func.h"
 
@@ -31,5 +32,30 @@ char * strstrci(char const * needle, int const * needle_lps, char const * haysta
 }
 
 int main() {
+    unsigned sz1, sz2, i;
+    char * haystack, *needle;
+    int res = scanf("%u", &sz1);
+    assert(res == 1);
+
+    haystack = calloc(sz1 + 1, sizeof(char));
+    for (i = 0; i < sz1; ++i) {
+        res = scanf("%c", &haystack[i]);
+        assert(res == 1);
+    }
+
+    haystack[i] = '\0';
+
+    res = scanf("%u", &sz2);
+    assert(res == 1);
+
+    needle = calloc(sz2 + 1, sizeof(char));
+    for (i = 0; i < sz2; ++i) {
+        res = scanf("%c", &needle[i]);
+        assert(res == 1);
+    }
+
+    needle[i] = '\0';
+
+
     return 0;
 }

e899d6562a01d0b3e088eaecc20288d078499f3f
Nikolai Popesku <nic1k@yandex.com>
2026-07-29 13:07:47 +0800

    5_strings_and_automata/9_problem_hwk.c

--- 5_strings_and_automata/9_problem_hwk.c
+++ 5_strings_and_automata/9_problem_hwk.c
@@ -1,5 +1,7 @@
 #include <assert.h>
 #include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "func.h"
 
@@ -41,4 +43,60 @@ char *strstrci(char const *needle, int const *needle_lps, char const *haystack) 
     }
 
     return NULL;
+}
+
+int main() {
+    unsigned sz1, sz2, i;
+    char *haystack, *needle, *pos;
+    int res = scanf("%u", &sz1);
+    int *needle_lps, counter;
+    assert(res == 1);
+
+    getchar();
+
+    haystack = calloc(sz1 + 1, sizeof(char));
+    for (i = 0; i < sz1; ++i) {
+        res = scanf("%c", &haystack[i]);
+        assert(res == 1);
+    }
+
+    haystack[i] = '\0';
+
+    getchar();
+
+    res = scanf("%u", &sz2);
+    assert(res == 1);
+
+    getchar();
+
+    needle = calloc(sz2 + 1, sizeof(char));
+    for (i = 0; i < sz2; ++i) {
+        res = scanf("%c", &needle[i]);
+        assert(res == 1);
+    }
+
+    needle[i] = '\0';
+
+    needle_lps = calloc(strlen(needle), sizeof(int));
+    patpreproc(needle, needle_lps);
+
+    counter = 0;
+    pos = haystack;
+
+    do {
+        pos = strstrci(needle, needle_lps, pos);
+        if (pos != NULL) {
+            ++counter;
+            ++pos;
+        }
+    } while (pos != NULL);
+
+    free(needle_lps);
+    free(needle);
+    free(haystack);
+
+    printf("%d\n", counter);
+
+
+    return 0;
 }

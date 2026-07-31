44a9315b77883193e8f7801bd9716e9a0e90749b
Nikolai Popesku <nic1k@yandex.com>
2026-07-31 12:10:27 +0800

    6_assembler_and_architecture/2_problem_as2.c

--- 6_assembler_and_architecture/2_problem_as2.c
+++ 6_assembler_and_architecture/2_problem_as2.c
@@ -5,12 +5,33 @@
 
 int main() {
     unsigned val;
+
     while (scanf("%x", &val) == 1) {
         if (val >> 7 == 0) {
             printf("MOVI %d\n", val);
             continue;
         }
 
+        switch (val >> 4) {
+            case 8:
+                printf("ADD ");
+                break;
+            case 9:
+                printf("SUB ");
+                break;
+            case 10:
+                printf("MUL ");
+                break;
+            case 11:
+                printf("DIV ");
+                break;
+            case 12:
+                if ((val & (1 << 3)) == 1) {
+                    printf("OUT ");
+                } else {
+                    printf("IN ");
+                }
+        }
 
     }
     return 0;

77e0654c061aeb49b9cd490bfbe948bed04bce99
Nikolai Popesku <nic1k@yandex.com>
2026-07-31 12:17:08 +0800

    6_assembler_and_architecture/2_problem_as2.c

--- 6_assembler_and_architecture/2_problem_as2.c
+++ 6_assembler_and_architecture/2_problem_as2.c
@@ -3,8 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+enum Register {
+    A = 0,
+    B = 1,
+    C = 10,
+    D = 11
+};
+
 int main() {
     unsigned val;
+    enum Register first;
+    enum Register second;
 
     while (scanf("%x", &val) == 1) {
         if (val >> 7 == 0) {
@@ -12,18 +21,21 @@ int main() {
             continue;
         }
 
+        first = (val & (11 << 2)) >> 2;
+        second = val & 11;
+
         switch (val >> 4) {
             case 8:
-                printf("ADD ");
+                printf("ADD %c, %c", first, second);
                 break;
             case 9:
-                printf("SUB ");
+                printf("SUB %c, %c", first, second);
                 break;
             case 10:
-                printf("MUL ");
+                printf("MUL %c, %c", first, second);
                 break;
             case 11:
-                printf("DIV ");
+                printf("DIV %c, %c", first, second);
                 break;
             case 12:
                 if ((val & (1 << 3)) == 1) {

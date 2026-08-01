aa0c1d2311a4783e47d7d1dac9d060afb7f8754e
Nikolai Popesku <nic1k@yandex.com>
2026-08-01 13:13:10 +0800

    6_assembler_and_architecture/1_problem_as.c

--- 6_assembler_and_architecture/1_problem_as.c
+++ 6_assembler_and_architecture/1_problem_as.c
@@ -11,6 +11,13 @@ enum Register {
     D = 3
 };
 
+enum Opcode {
+    OP_ADD = 8,
+    OP_SUB = 9,
+    OP_MUL = 10,
+    OP_DIV = 11
+};
+
 
 int main() {
     char ch;
@@ -21,7 +28,26 @@ int main() {
 
     regex_t re_movi, re_out, re_in, re_add, re_sub, re_mul, re_div;
     regmatch_t matches[5];
-    // regex_t re_arr[4];
+    unsigned k;
+    int matched, first_token = 1;
+
+    struct {
+        regex_t *re;
+        enum Opcode op;
+    } two_op[] = {
+        {&re_add, OP_ADD},
+        {&re_sub, OP_SUB},
+        {&re_mul, OP_MUL},
+        {&re_div, OP_DIV},
+    };
+
+    struct {
+        regex_t *re;
+        unsigned base;
+    } one_op[] = {
+        {&re_in, 0xC0},
+        {&re_out, 0xC4},
+    };
 
     regcomp(&re_movi, "^(MOVI) ([0-9]+)$", REG_EXTENDED);
     regcomp(&re_out, "^(OUT) ([A-D]{1})$", REG_EXTENDED);
@@ -30,15 +56,6 @@ int main() {
     regcomp(&re_sub, "^(SUB) ([A-D]{1})(,[[:space:]]?)([A-D]{1})$", REG_EXTENDED);
     regcomp(&re_mul, "^(MUL) ([A-D]{1})(,[[:space:]]?)([A-D]{1})$", REG_EXTENDED);
     regcomp(&re_div, "^(DIV) ([A-D]{1})(,[[:space:]]?)([A-D]{1})$", REG_EXTENDED);
-
-    // re_arr[0] = re_movi;
-    // re_arr[1] = re_out;
-    // re_arr[2] = re_in;
-    // re_arr[0] = re_add;
-    // re_arr[1] = re_sub;
-    // re_arr[2] = re_mul;
-    // re_arr[3] = re_div;
-
 
     do {
         len = 0;
@@ -57,19 +74,57 @@ int main() {
 
         buf[len] = '\0';
 
+        if (len == 0) {
+            free(buf);
+            break;
+        }
 
-        if (regexec(&re_add, buf, 5, matches, 0) == 0) {
+        matched = 0;
+
+        if (regexec(&re_movi, buf, 3, matches, 0) == 0) {
             regmatch_t g2 = matches[2];
-            regmatch_t g4 = matches[4];
-            val  = 8 << 4;
-            reg = buf[g2.rm_so] - 'A';
-            val = val + (reg << 2);
-            val = val + (buf[g4.rm_so] - 'A');
-            printf("0x%x", val);
+            val = strtoul(buf + g2.rm_so, NULL, 10) & 0x7F;
+            matched = 1;
+        } else {
+            for (k = 0; k < 2; ++k) {
+                if (regexec(one_op[k].re, buf, 3, matches, 0) == 0) {
+                    regmatch_t g2 = matches[2];
+                    reg = buf[g2.rm_so] - 'A';
+                    val = one_op[k].base | reg;
+                    matched = 1;
+                    break;
+                }
+            }
+        }
+
+        if (!matched) {
+            for (k = 0; k < 4; ++k) {
+                if (regexec(two_op[k].re, buf, 5, matches, 0) == 0) {
+                    regmatch_t g2 = matches[2];
+                    regmatch_t g4 = matches[4];
+                    val  = two_op[k].op << 4;
+                    reg = buf[g2.rm_so] - 'A';
+                    val = val + (reg << 2);
+                    val = val + (buf[g4.rm_so] - 'A');
+                    matched = 1;
+                    break;
+                }
+            }
+        }
+
+        if (matched) {
+            printf(first_token ? "0x%02x" : " 0x%02x", val);
+            first_token = 0;
+        } else {
+            printf(first_token ? "ERROR" : " ERROR");
+            free(buf);
+            break;
         }
 
         free(buf);
     } while (len > 0);
+
+    printf("\n");
 
     regfree(&re_movi);
     regfree(&re_out);

02956049e31658ef4e94d8a135687af69cfcbf19
Nikolai Popesku <nic1k@yandex.com>
2026-08-01 11:55:19 +0800

    6_assembler_and_architecture/1_problem_as.c

--- 6_assembler_and_architecture/1_problem_as.c
+++ 6_assembler_and_architecture/1_problem_as.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,6 +7,10 @@ int main() {
     char ch;
     size_t cap = 16, len = 0;
     char *buf = calloc(cap, sizeof(char)), *new_buf;
+
+    regex_t re;
+    int ret = regcomp(&re, "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", REG_EXTENDED);
+    assert(ret == 0);
 
     while ((ch = getchar()) != EOF && ch != '\n') {
         if (len + 1 >= cap) {
@@ -16,6 +21,9 @@ int main() {
         }
         buf[len++] = ch;
     }
+
+    buf[len] = '\0';
+
 
     return 0;
 }

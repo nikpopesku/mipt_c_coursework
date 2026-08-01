6d92b0e52c9c662df24f905629b533fcead30ac2
Nikolai Popesku <nic1k@yandex.com>
2026-08-01 11:54:44 +0800

    6_assembler_and_architecture/1_problem_as.c

--- 6_assembler_and_architecture/1_problem_as.c
+++ 6_assembler_and_architecture/1_problem_as.c
@@ -1,5 +1,21 @@
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+    char ch;
+    size_t cap = 16, len = 0;
+    char *buf = calloc(cap, sizeof(char)), *new_buf;
+
+    while ((ch = getchar()) != EOF && ch != '\n') {
+        if (len + 1 >= cap) {
+            cap *= 2;
+            new_buf = realloc(buf, cap);
+            buf = new_buf;
+            assert(buf != NULL);
+        }
+        buf[len++] = ch;
+    }
+
     return 0;
 }

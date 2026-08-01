f4097601db6996bde1f1fcc2cab17cbc9f8acd81
Nikolai Popesku <nic1k@yandex.com>
2026-08-01 13:05:07 +0800

    6_assembler_and_architecture/1_problem_as.c

--- 6_assembler_and_architecture/1_problem_as.c
+++ 6_assembler_and_architecture/1_problem_as.c
@@ -4,31 +4,40 @@
 #include <stdlib.h>
 #include <string.h>
 
+enum Register {
+    A = 0,
+    B = 1,
+    C = 2,
+    D = 3
+};
+
+
 int main() {
     char ch;
     size_t cap = 16, len = 0;
     char *buf = NULL, *new_buf;
-    unsigned i, j;
+    unsigned val;
+    enum Register reg;
 
     regex_t re_movi, re_out, re_in, re_add, re_sub, re_mul, re_div;
     regmatch_t matches[5];
-    regex_t re_arr[7];
+    // regex_t re_arr[4];
 
-    regcomp(&re_movi, "^(MOVI )([0-9]+)$", REG_EXTENDED);
-    regcomp(&re_out, "^(OUT )([A-D]{1})$", REG_EXTENDED);
-    regcomp(&re_in, "^(IN )([A-D]{1})$", REG_EXTENDED);
-    regcomp(&re_add, "^(ADD )([A-D]{1})(,[[:space:]]?)([A-D]{1})$", REG_EXTENDED);
-    regcomp(&re_sub, "^(SUB )([A-D]{1})(,[[:space:]]?)([A-D]{1})$", REG_EXTENDED);
-    regcomp(&re_mul, "^(MUL )([A-D]{1})(,[[:space:]]?)([A-D]{1})$", REG_EXTENDED);
-    regcomp(&re_div, "^(DIV )([A-D]{1})(,[[:space:]]?)([A-D]{1})$", REG_EXTENDED);
+    regcomp(&re_movi, "^(MOVI) ([0-9]+)$", REG_EXTENDED);
+    regcomp(&re_out, "^(OUT) ([A-D]{1})$", REG_EXTENDED);
+    regcomp(&re_in, "^(IN) ([A-D]{1})$", REG_EXTENDED);
+    regcomp(&re_add, "^(ADD) ([A-D]{1})(,[[:space:]]?)([A-D]{1})$", REG_EXTENDED);
+    regcomp(&re_sub, "^(SUB) ([A-D]{1})(,[[:space:]]?)([A-D]{1})$", REG_EXTENDED);
+    regcomp(&re_mul, "^(MUL) ([A-D]{1})(,[[:space:]]?)([A-D]{1})$", REG_EXTENDED);
+    regcomp(&re_div, "^(DIV) ([A-D]{1})(,[[:space:]]?)([A-D]{1})$", REG_EXTENDED);
 
-    re_arr[0] = re_movi;
-    re_arr[1] = re_out;
-    re_arr[2] = re_in;
-    re_arr[3] = re_add;
-    re_arr[4] = re_sub;
-    re_arr[5] = re_mul;
-    re_arr[6] = re_div;
+    // re_arr[0] = re_movi;
+    // re_arr[1] = re_out;
+    // re_arr[2] = re_in;
+    // re_arr[0] = re_add;
+    // re_arr[1] = re_sub;
+    // re_arr[2] = re_mul;
+    // re_arr[3] = re_div;
 
 
     do {
@@ -49,19 +58,14 @@ int main() {
         buf[len] = '\0';
 
 
-        for (i = 0; i < 7; ++i) {
-            if (regexec(&re_arr[i], buf, 5, matches, 0) == 0) {
-                regmatch_t g1 = matches[1];
-                regmatch_t g2 = matches[2];
-                regmatch_t g3 = matches[3];
-                regmatch_t g4 = matches[4];
-
-                for (j = g1.rm_so; j < g1.rm_eo; ++j) printf("%c", buf[j]);
-                for (j = g2.rm_so; j < g2.rm_eo; ++j) printf("%c", buf[j]);
-                for (j = g3.rm_so; j < g3.rm_eo; ++j) printf("%c", buf[j]);
-                for (j = g4.rm_so; j < g4.rm_eo; ++j) printf("%c", buf[j]);
-                printf("\n");
-            }
+        if (regexec(&re_add, buf, 5, matches, 0) == 0) {
+            regmatch_t g2 = matches[2];
+            regmatch_t g4 = matches[4];
+            val  = 8 << 4;
+            reg = buf[g2.rm_so];
+            val = val + (reg << 2);
+            val = val + buf[g4.rm_so];
+            printf("%x", val);
         }
 
         free(buf);

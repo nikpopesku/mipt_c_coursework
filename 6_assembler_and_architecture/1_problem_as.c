4f205bda831bb69b662a64fc12c4ecb6a679de7b
Nikolai Popesku <nic1k@yandex.com>
2026-08-01 12:20:35 +0800

    6_assembler_and_architecture/1_problem_as.c

--- 6_assembler_and_architecture/1_problem_as.c
+++ 6_assembler_and_architecture/1_problem_as.c
@@ -7,17 +7,21 @@
 int main() {
     char ch;
     size_t cap = 16, len = 0;
-    char *buf = calloc(cap, sizeof(char)), *new_buf;
+    char *buf, *new_buf;
     unsigned i;
 
     regex_t re_movi, re_out;
     regmatch_t matches[2];
     int res = regcomp(&re_movi, "^MOVI ([0-9]+)$", REG_EXTENDED);
     assert(res == 0);
-    res = regcomp(&re_out, "^OUT ([A-Z]{1})$", REG_EXTENDED);
+    res = regcomp(&re_out, "^OUT ([A-D]{1})$", REG_EXTENDED);
     assert(res == 0);
 
     do {
+        len = 0;
+        cap = 16;
+        buf = calloc(cap, sizeof(char));
+
         while ((ch = getchar()) != EOF && ch != '\n') {
             if (len + 1 >= cap) {
                 cap *= 2;
@@ -40,9 +44,19 @@ int main() {
             }
             printf("\n");
         }
-    } while (buf != NULL);
+        if (regexec(&re_out, buf, 2, matches, 0) == 0) {
+            regmatch_t g = matches[1];
 
-    regfree(&re);
+            printf("OUT ");
+            for (i = g.rm_so; i < g.rm_eo; ++i) {
+                printf("%c", buf[i]);
+            }
+            printf("\n");
+        }
+    } while (len > 0);
+
+    regfree(&re_movi);
+    regfree(&re_out);
     free(buf);
 
     return 0;

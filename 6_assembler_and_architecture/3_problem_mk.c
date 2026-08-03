9f82ae7d9f8a1d0228ef210e06dd6ffd8b251235
Nikolai Popesku <nic1k@yandex.com>
2026-08-03 11:54:16 +0800

    6_assembler_and_architecture/3_problem_mk.c

--- 6_assembler_and_architecture/3_problem_mk.c
+++ 6_assembler_and_architecture/3_problem_mk.c
@@ -14,18 +14,30 @@ int main() {
     unsigned A, B, C, D;
     char ch;
     size_t cap = 16, len = 0;
+    size_t cap_num = 16, len_num = 0;
     char *buf = calloc(cap, sizeof(char)), *new_buf;
+    char *buf_num = calloc(cap, sizeof(char)), *new_buf_num;
     enum Register first;
     enum Register second;
 
-    while ((ch = getchar()) != EOF && ch != '\n') {
+    while (scanf("%x", &val) == 1) {
         if (len + 1 >= cap) {
             cap *= 2;
             new_buf = realloc(buf, cap);
             buf = new_buf;
             assert(buf != NULL);
         }
-        buf[len++] = (char) ch;
+        buf[len++] = val;
+    }
+
+    while (scanf("%d", &val) == 1) {
+        if (len_num + 1 >= cap_num) {
+            cap *= 2;
+            new_buf_num = realloc(buf_num, cap_num);
+            buf_num = new_buf_num;
+            assert(buf_num != NULL);
+        }
+        buf_num[len_num++] = val;
     }
 
     while (scanf("%x", &val) == 1) {

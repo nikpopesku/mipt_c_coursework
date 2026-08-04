fcd418eeb2d786875175bde667a9bc2cdda793aa
Nikolai Popesku <nic1k@yandex.com>
2026-08-04 12:36:45 +0800

    6_assembler_and_architecture/4_problem_agf.c

--- 6_assembler_and_architecture/4_problem_agf.c
+++ 6_assembler_and_architecture/4_problem_agf.c
@@ -1,6 +1,28 @@
 #include <assert.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+int foo(int x, int y) {
+    int eax, edx;
+    int edi = x;
+    int esi = y;
+    eax = edi;
+    eax = eax * edi * edi;
+
+    if (eax < 0) {
+        esi = esi * esi;
+        edi = edi + edi;
+        edx = 17;
+        edx = edx - edi;
+        edx = edx - esi;
+        eax = eax + edx;
+
+        return eax;
+    }
+
+    return eax;
+}
 
 
 int main() {

57d58e5ee31d335cddd2a27c89cf7d4af0e16821
Nikolai Popesku <nic1k@yandex.com>
2026-08-10 12:17:21 +0800

    6_assembler_and_architecture/prime.c

--- 6_assembler_and_architecture/prime.c
+++ 6_assembler_and_architecture/prime.c
@@ -2,4 +2,10 @@ int prime(int x) {
     if (x <= 1) return 0;
     if (x <= 3) return 1;
     if (x % 2 == 0) return 0;
+
+    for (int i = 5; i * i <= x; i += 2) {
+        if (x % i == 0) return 0;
+    }
+
+    return 1;
 }

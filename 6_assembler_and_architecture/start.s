d8e6e37a20ae82ff8a12919107c5c2e1bb593eba
Nikolai Popesku <nic1k@yandex.com>
2026-08-10 12:46:32 +0800

    start.s

--- 6_assembler_and_architecture/start.s
+++ 6_assembler_and_architecture/start.s
@@ -8,13 +8,13 @@
 .LFB0:
 	.cfi_startproc
 	cmp	edi, 1
-	jbe	.L5
+	jbe	.composite
 	cmp	edi, 3
-	jbe	.L6
+	jbe	.prime
 	test	dil, 1
-	je	.L5
+	je	.composite
 	cmp	edi, 24
-	jbe	.L6
+	jbe	.prime
 	mov	ecx, 5
 	jmp	.L7
 	.p2align 5
@@ -25,19 +25,19 @@
 	mov	eax, ecx
 	imul	eax, ecx
 	cmp	edi, eax
-	jb	.L6
+	jb	.prime
 .L7:
 	mov	eax, edi
 	xor	edx, edx
 	div	ecx
 	test	edx, edx
 	jne	.L16
-.L5:
+.composite:
 	xor	eax, eax
 	ret
 	.p2align 4,,10
 	.p2align 3
-.L6:
+.prime:
 	mov	eax, 1
 	ret
 	.cfi_endproc

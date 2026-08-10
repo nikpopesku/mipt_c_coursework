0b5a07b04cd941ea31f758d69b781166965b3686
Nikolai Popesku <nic1k@yandex.com>
2026-08-10 12:52:41 +0800

    start.s

--- 6_assembler_and_architecture/start.c
+++ 6_assembler_and_architecture/start.c
@@ -3,7 +3,7 @@ int start(unsigned x) {
     if (x <= 3) return 1;
     if (x % 2 == 0) return 0;
 
-    for (int i = 5; i * i <= x; i += 2) {
+    for (int i = 3; i * i <= x; i += 2) {
         if (x % i == 0) return 0;
     }
 

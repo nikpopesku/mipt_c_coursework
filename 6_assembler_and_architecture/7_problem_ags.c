09cb9db59a0ea8e3f4c95f36e8bbc8e0c87a6207
Nikolai Popesku <nic1k@yandex.com>
2026-08-12 11:54:42 +0800

    6_assembler_and_architecture/7_problem_ags.c

--- 6_assembler_and_architecture/7_problem_ags.c
+++ 6_assembler_and_architecture/7_problem_ags.c
@@ -2,47 +2,47 @@
 .globl  caller
 .type caller, @function
 caller:
-push  r13
-push  r12
-push  rbp
-push  rbx
-sub rsp, 8
-mov r12d, 1
-test  esi, esi
-je  .L1
-mov eax, edi
-mov ebx, esi
-mov r13d, edx
-mov edx, 0
-div r13d
-mov rbp, rdx
-mov r12d, 1
-jmp .L5
+    push  r13
+    push  r12
+    push  rbp
+    push  rbx
+    sub rsp, 8
+    mov r12d, 1
+    test  esi, esi
+    je  .L1
+    mov eax, edi
+    mov ebx, esi
+    mov r13d, edx
+    mov edx, 0
+    div r13d
+    mov rbp, rdx
+    mov r12d, 1
+    jmp .L5
 .L3:
-mov edi, ebp
-mov edx, r13d
-mov esi, ebp
-call  callee
-mov ebp, eax
-shr ebx
+    mov edi, ebp
+    mov edx, r13d
+    mov esi, ebp
+    call  callee
+    mov ebp, eax
+    shr ebx
 .L4:
-test  ebx, ebx
-je  .L1
+    test  ebx, ebx
+    je  .L1
 .L5:
-test  bl, 1
-je  .L3
-mov edx, r13d
-mov esi, ebp
-mov edi, r12d
-call  callee
-mov r12d, eax
-sub ebx, 1
-jmp .L4
+    test  bl, 1
+    je  .L3
+    mov edx, r13d
+    mov esi, ebp
+    mov edi, r12d
+    call  callee
+    mov r12d, eax
+    sub ebx, 1
+    jmp .L4
 .L1:
-mov eax, r12d
-add rsp, 8
-pop rbx
-pop rbp
-pop r12
-pop r13
-ret
+    mov eax, r12d
+    add rsp, 8
+    pop rbx
+    pop rbp
+    pop r12
+    pop r13
+    ret

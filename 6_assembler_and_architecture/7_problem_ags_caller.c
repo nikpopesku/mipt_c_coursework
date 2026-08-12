e5fcc8d562211546a21d1533f85f02250eb23a0b
Nikolai Popesku <nic1k@yandex.com>
2026-08-12 12:35:23 +0800

    7_problem_ags_caller.c

--- 6_assembler_and_architecture/7_problem_ags_caller.c
+++ 6_assembler_and_architecture/7_problem_ags_caller.c
@@ -1,35 +1,35 @@
-int caller() {
+int caller(int x, int y, int z) {
         rsp = rsp - 8;
         r12d = 1;
 
-        if (esi == 0) goto L1;
+        if (y == 0) goto L1;
 
-        eax = edi;
-        ebx = esi;
-        r13d = edx;
-        edx = 0;
-        edx = eax % r13d; eax = eax / r13d;
-        rbp = rdx;
+        eax = x;
+        ebx = y;
+        r13d = z;
+        z = 0;
+        z = eax % r13d; eax = eax / r13d;
+        rbp = z;
         r12d = 1;
         goto L5;
     L3:
-        edi = ebp;
-        edx = r13d;
-        esi = ebp;
-        callee(edi, esi, edx);
-        ebp = eax
-        ebx = ebx >> 1
+        x = ebp;
+        z = r13d;
+        y = ebp;
+        callee(x, y, z);
+        ebp = eax;
+        ebx = ebx >> 1;
     L4:
         if (ebx == 0) goto L1;
     L5:
         if (bl % 2 == 0) goto L3;
-        edx = r13d;
-        esi = ebp;
-        edi = r12d;
-        callee(edi, esi, edx);
+        z = r13d;
+        y = ebp;
+        x = r12d;
+        callee(x, y, z);
         r12d = eax;
         ebx -= 1;
-        goto L4
+        goto L4;
     L1:
         eax = r12d;
         rsp += 8;

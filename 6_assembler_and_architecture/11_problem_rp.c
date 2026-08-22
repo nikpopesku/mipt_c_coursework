78d3dbb430412b6fdde5e407f6f0068526dd957e
Nikolai Popesku <nic1k@yandex.com>
2026-08-22 14:11:47 +0800

    11_problem_rp.c

--- 6_assembler_and_architecture/11_problem_rp.c
+++ 6_assembler_and_architecture/11_problem_rp.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <fenv.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 static unsigned as_uint(const float f) {
@@ -20,10 +21,10 @@ int main() {
     assert(res == 1);
 
     fesetround(FE_DOWNWARD);
-    f_down = (float) numerator / denominator;
+    f_down = (float) abs(numerator) / denominator;
 
     fesetround(FE_UPWARD);
-    f_up = (float) numerator / denominator;
+    f_up = (float) abs(numerator) / denominator;
 
     if (f_down != f_up) {
         printf("0x%x 0x%x 0x%x 0x%x", (as_uint(f_down) >> 23) & ((1U << 8) - 1), as_uint(f_down) & frac, (as_uint(f_up) >> 23) & ((1U << 8) - 1), as_uint(f_up) & frac);

db439f2489ac5b9a6400c5943f0cb78895e22cb5
Nikolai Popesku <nic1k@yandex.com>
2026-08-22 13:47:42 +0800

    11_problem_rp.c

--- 6_assembler_and_architecture/11_problem_rp.c
+++ 6_assembler_and_architecture/11_problem_rp.c
@@ -1,7 +1,18 @@
 #include <assert.h>
+#include <fenv.h>
 #include <stdio.h>
-#include <string.h>
 
 
 int main() {
+    unsigned numerator, denominator;
+    int res = scanf("%u", &numerator);
+    assert(res == 1);
+    res = scanf("%u", &denominator);
+    assert(res == 1);
+
+    fesetround(FE_DOWNWARD);
+    printf("%f", (float) numerator / denominator);
+
+    fesetround(FE_UPWARD);
+    printf("%f", (float) numerator / denominator);
 }

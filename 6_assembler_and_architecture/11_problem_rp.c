33c1ccf26aa18e4e85dd50989982770748811c7a
Nikolai Popesku <nic1k@yandex.com>
2026-08-22 13:51:39 +0800

    11_problem_rp.c

--- 6_assembler_and_architecture/11_problem_rp.c
+++ 6_assembler_and_architecture/11_problem_rp.c
@@ -1,18 +1,32 @@
 #include <assert.h>
 #include <fenv.h>
 #include <stdio.h>
+#include <string.h>
 
+static unsigned as_uint(const float f) {
+    unsigned u;
+    memcpy(&u, &f, sizeof(unsigned));
+    return u;
+}
 
 int main() {
     unsigned numerator, denominator;
+    float f_down, f_up;
     int res = scanf("%u", &numerator);
     assert(res == 1);
     res = scanf("%u", &denominator);
     assert(res == 1);
 
     fesetround(FE_DOWNWARD);
-    printf("%f", (float) numerator / denominator);
+    f_down = (float) numerator / denominator;
 
     fesetround(FE_UPWARD);
-    printf("%f", (float) numerator / denominator);
+    f_up = (float) numerator / denominator;
+
+    if (f_down != f_up) {
+        printf("%x %x", as_uint(f_down), as_uint(f_up));
+    } else {
+        printf("%x", as_uint(f_down));
+    }
+
 }

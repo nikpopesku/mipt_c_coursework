799921e556482b50598733bb4dac8c72ec1bac75
Nikolai Popesku <nic1k@yandex.com>
2026-08-22 13:31:04 +0800

    6_assembler_and_architecture/10_problem_ex.c

--- 6_assembler_and_architecture/10_problem_ex.c
+++ 6_assembler_and_architecture/10_problem_ex.c
@@ -2,15 +2,33 @@
 #include <stdio.h>
 #include <string.h>
 
+
 static unsigned as_uint(const float f) {
     unsigned u;
     memcpy(&u, &f, sizeof(unsigned));
     return u;
 }
 
+static float as_float(const unsigned u) {
+    float f;
+    memcpy(&f, &u, sizeof(float));
+
+    return f;
+}
+
 int main() {
     float num;
+    unsigned u, i, e;
     int res = scanf("%f", &num);
     assert(res == 1);
 
+    u = as_uint(num);
+    e = 1;
+
+    for (i = 1; i < 23; i += 2) {
+        e <<= i;
+        u = u ^ e;
+    }
+
+    printf("%f\n", as_float(u));
 }

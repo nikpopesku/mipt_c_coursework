48dcfeae3d8627eec3df561f686204460b7f5ca2
Nikolai Popesku <nic1k@yandex.com>
2026-08-22 14:06:49 +0800

    11_problem_rp.c

--- 6_assembler_and_architecture/11_problem_rp.c
+++ 6_assembler_and_architecture/11_problem_rp.c
@@ -10,10 +10,11 @@ static unsigned as_uint(const float f) {
 }
 
 int main() {
-    unsigned numerator, denominator;
+    unsigned denominator;
+    int numerator;
     unsigned frac = (1U << 23) - 1;
     float f_down, f_up;
-    int res = scanf("%u", &numerator);
+    int res = scanf("%d", &numerator);
     assert(res == 1);
     res = scanf("%u", &denominator);
     assert(res == 1);
@@ -25,8 +26,8 @@ int main() {
     f_up = (float) numerator / denominator;
 
     if (f_down != f_up) {
-        printf("0x%x 0x%x 0x%x 0x%x", (as_uint(f_down) >> 23) & ((1U << 10) - 1), as_uint(f_down) & frac, (as_uint(f_up) >> 23) & ((1U << 10) - 1), as_uint(f_up) & frac);
+        printf("0x%x 0x%x 0x%x 0x%x", (as_uint(f_down) >> 23) & ((1U << 8) - 1), as_uint(f_down) & frac, (as_uint(f_up) >> 23) & ((1U << 8) - 1), as_uint(f_up) & frac);
     } else {
-        printf("0x%x 0x%x", as_uint(f_down) & frac, (as_uint(f_down) >> 23) & ((1U << 10) - 1));
+        printf("0x%x 0x%x", (as_uint(f_down) >> 23) & ((1U << 8) - 1), as_uint(f_down) & frac);
     }
 }

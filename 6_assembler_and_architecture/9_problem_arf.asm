9d05ca8060bff89d170c9713c3850cebf94fdb4a
Nikolai Popesku <nic1k@yandex.com>
2026-08-24 13:03:49 +0800

    6_assembler_and_architecture/9_problem_arf.asm

--- 6_assembler_and_architecture/9_problem_arf.asm
+++ 6_assembler_and_architecture/9_problem_arf.asm
@@ -3,15 +3,15 @@
 foo:
   mov       edi, edi   ; can be ignored
   pxor      xmm1, xmm1 ; xmm1 = 0
-  sub       rsp, 24    ;rsp pointer - i can ignore in this func
+  sub       rsp, 24    ;rsp -= 24
   cvtsi2sd  xmm1, rdi  ; xmm1 = static_cast<float>(rdi)
   movapd    xmm0, xmm1 ; xmm0 = xmm1
-  movsd     QWORD PTR [rsp], xmm1
-  call      log      ; log(xmm0) probably
-  movsd     QWORD PTR [rsp+8], xmm0
-  movsd     xmm0, QWORD PTR [rsp]
-  call      log;  log(xmm0)
-  call      log   log(xmm0)
+  movsd     QWORD PTR [rsp], xmm1; rsp = xmm1
+  call      log      ; xmm0 = log(xmm0) probably
+  movsd     QWORD PTR [rsp+8], xmm0; *(rsp + 1) = xmm0
+  movsd     xmm0, QWORD PTR [rsp] ; xmm0 = rsp
+  call      log     ;  xmm0 = log(xmm0)
+  call      log     ;  xmm0 = log(xmm0)
   addsd     xmm0, QWORD PTR [rsp+8]  ; xmm0 += smth on the stack
   mulsd     xmm0, QWORD PTR [rsp]    ; xmm0 *= smth on the stack
   call      round;      ;round(xmm0)

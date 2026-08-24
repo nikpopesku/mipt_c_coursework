fec639289cdf818983d96e31d92f362d06deb238
Nikolai Popesku <nic1k@yandex.com>
2026-08-24 13:12:10 +0800

    9_problem_arf.asm 9_problem_arf.c

--- 6_assembler_and_architecture/9_problem_arf.c
+++ 6_assembler_and_architecture/9_problem_arf.c
@@ -5,10 +5,10 @@ int foo(double rdi) {
     double xmm1 = 0;
     double xmm0;
     int rax;
-    int * rsp;
+    int *rsp;
     xmm1 = rdi;
     xmm0 = xmm1;
-    *(double*)rsp = xmm1;
+    *rsp = xmm1;
     xmm0 = log(xmm0);
     *(rsp + 1) = xmm0;
     xmm0 = *rsp;

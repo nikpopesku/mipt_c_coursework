20f70979e572be9b00543d8acd766868f5fff393
Nikolai Popesku <nic1k@yandex.com>
2026-08-31 11:47:56 +0800

    6_assembler_and_architecture/8_problem_agr.asm

--- 6_assembler_and_architecture/8_problem_agr.asm
+++ 6_assembler_and_architecture/8_problem_agr.asm
@@ -1,12 +1,12 @@
 foo:
-        slli    a5,a1,32  ; a5 = a1 >> 32 i.e. a5 = 0 probably
-        srli    t4,a5,30  ; t4 = a5 << 30
+        slli    a5,a1,32  ; a5 = a1 << 32 i.e. a5 = 0 probably
+        srli    t4,a5,30  ; t4 = a5 >> 30
         add     t4,a0,t4  ; t4 = a0 + t4
         lw      t3,0(t4)  ; t3 = t4
         mv      a6,a0     ; a6 = a0
         sext.w  a7,a2     ; asm pseudoinstruction
         bgeu    a1,a2,.L7 ; if (a1 == a2) goto L7
-        slli    t1,a7,2   ; t1 = a7 >> 2
+        slli    t1,a7,2   ; t1 = a7 << 2
         add     a5,a0,t1  ; a5 = a0 + t1
         mv      a4,a7     ; a4 = a7
 .L6:
@@ -18,7 +18,7 @@ foo:
         addiw   a7,a7,-1  ; a7 = a7 - 1
         sw      a3,0(a2)  ; platform specific a3 = a2
         sw      a0,0(a5)  ; a0 = a5
-        slli    t1,a7,2   ; t1 = a7 >> 2
+        slli    t1,a7,2   ; t1 = a7 << 2
 .L5:
         addi    a5,a5,-4  ; a5 = a5 - 4
         bltu    a1,a4,.L6 ; if (a1 < a4) goto L6
@@ -30,7 +30,7 @@ foo:
         sw      a5,0(t4)  ; a0 = t4
         ret               ; return a0
 .L7:
-        slli    t1,a7,2   ; t1 = a7 >> 2
+        slli    t1,a7,2   ; t1 = a7 << 2
         add     a6,a6,t1  ; a6 = a6 + t1
         lw      a5,0(a6)  ; a5 = a6
         sw      t3,0(a6)  ; t3 = a6

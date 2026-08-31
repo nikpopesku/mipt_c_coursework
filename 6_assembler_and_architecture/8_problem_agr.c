8c85000ac89b91929ff1613cbfa51c6f914f9fdc
Nikolai Popesku <nic1k@yandex.com>
2026-08-31 12:20:24 +0800

    6_assembler_and_architecture/8_problem_agr.asm

--- 6_assembler_and_architecture/8_problem_agr.c
+++ 6_assembler_and_architecture/8_problem_agr.c
@@ -1,23 +1,26 @@
 #include <assert.h>
+#include <stdint.h>
 #include <stdlib.h>
 
-int foo() {
+int foo(int *a0, int a1, int a2) {
+    int a3, a4, a5, a6, a7, t1, t3;
+    int *t4;
     a5 = a1 << 32;
-    srlit4 = a5 >> 30;
+    t4 = a5 >> 30;
     t4 = a0 + t4;
     t3 = *t4;
     a6 = a0;
     a7 = (int32_t) a2;
     if (a1 >= a2) goto L7;
     t1 = a7 << 2;
-    a5 = a0 + t1;
+    a5 = *a0 + t1;
     a4 = a7;
 L6:
     a3 = *a5;
     a2 = a6 + t1;
     a4 = a4 - 1;
     if (a3 <= t3) goto L5;
-    a0 = *a2;
+    a0 = a2;
     a7 = a7 - 1;
     *a2 = a3;
     *a5 = a0;
@@ -27,9 +30,9 @@ L5:
     if (a1 < a4) goto L6;
     t3 = *t4;
     a6 = a6 + t1;
-    a5 = *a6;
+    a5 = a6;
     *a6 = t3;
-    a0 = (int32_t) a7;
+    a0 = &a7;
     *t4 = a5;
 
     return a0;

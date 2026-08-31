5c36e55e0f083b2f9f1284961318dc6a3fd52f5b
Nikolai Popesku <nic1k@yandex.com>
2026-08-31 12:32:21 +0800

    6_assembler_and_architecture/8_problem_agr.asm

--- 6_assembler_and_architecture/8_problem_agr.c
+++ 6_assembler_and_architecture/8_problem_agr.c
@@ -3,48 +3,48 @@
 #include <stdlib.h>
 
 int foo(int *a0, int a1, int a2) {
-    int a3, a4, a5, a6, a7, t1, t3;
-    int *t4;
-    int64_t tmp = (int64_t) a1 << 32;
-    int off = tmp >> 30;
-    t4 = a0 + off;
+    int a3, a4, a7, t1, t3, off, swp;
+    int64_t tmp;
+    int *a5, *a6, *pa2, *t4;
+
+    tmp = (int64_t) a1 << 32;
+    off = tmp >> 30;
+    t4 = (int *) ((char *) a0 + off);
     t3 = *t4;
     a6 = a0;
     a7 = (int) a2;
     if (a1 >= a2) goto L7;
     t1 = a7 << 2;
-    a5 = *a0 + t1;
+    a5 = (int *) ((char *) a0 + t1);
     a4 = a7;
 L6:
-    a3 = a5;
-    a2 = a6 + t1;
+    a3 = *a5;
+    pa2 = (int *) ((char *) a6 + t1);
     a4 = a4 - 1;
     if (a3 <= t3) goto L5;
-    a0 = &a2;
+    swp = *pa2;
     a7 = a7 - 1;
-    a2 = a3;
-    a5 = *a0;
+    *pa2 = a3;
+    *a5 = swp;
     t1 = a7 << 2;
 L5:
-    a5 = a5 - 4;
+    a5 = (int *) ((char *) a5 - 4);
     if (a1 < a4) goto L6;
     t3 = *t4;
-    a6 = a6 + t1;
-    a5 = a6;
-    a6 = t3;
-    a0 = &a7;
-    *t4 = a5;
+    a6 = (int *) ((char *) a6 + t1);
+    swp = *a6;
+    *a6 = t3;
+    *t4 = swp;
 
-    return *a0;
+    return a7;
 L7:
     t1 = a7 << 2;
-    a6 = a6 + t1;
-    a5 = a6;
-    a6 = t3;
-    a0 = &a2;
-    *t4 = a5;
+    a6 = (int *) ((char *) a6 + t1);
+    swp = *a6;
+    *a6 = t3;
+    *t4 = swp;
 
-    return *a0;
+    return a2;
 }
 
 main() {

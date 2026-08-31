bb49614d2cfa310acf629b3194384600b241bc1f
Nikolai Popesku <nic1k@yandex.com>
2026-08-31 12:23:12 +0800

    6_assembler_and_architecture/8_problem_agr.asm

--- 6_assembler_and_architecture/8_problem_agr.c
+++ 6_assembler_and_architecture/8_problem_agr.c
@@ -5,25 +5,26 @@
 int foo(int *a0, int a1, int a2) {
     int a3, a4, a5, a6, a7, t1, t3;
     int *t4;
-    a5 = a1 << 32;
+    int64_t tmp = (int64_t) a1 << 32;
+    a5 = tmp >> 30;
     t4 = a5 >> 30;
     t4 = a0 + t4;
     t3 = *t4;
     a6 = a0;
-    a7 = (int32_t) a2;
+    a7 = (int) a2;
     if (a1 >= a2) goto L7;
     t1 = a7 << 2;
     a5 = *a0 + t1;
     a4 = a7;
 L6:
-    a3 = *a5;
+    a3 = a5;
     a2 = a6 + t1;
     a4 = a4 - 1;
     if (a3 <= t3) goto L5;
-    a0 = a2;
+    a0 = &a2;
     a7 = a7 - 1;
-    *a2 = a3;
-    *a5 = a0;
+    a2 = a3;
+    a5 = *a0;
     t1 = a7 << 2;
 L5:
     a5 = a5 - 4;
@@ -31,20 +32,20 @@ L5:
     t3 = *t4;
     a6 = a6 + t1;
     a5 = a6;
-    *a6 = t3;
+    a6 = t3;
     a0 = &a7;
     *t4 = a5;
 
-    return a0;
+    return *a0;
 L7:
     t1 = a7 << 2;
     a6 = a6 + t1;
-    a5 = *a6;
-    *a6 = t3;
-    a0 = a2;
+    a5 = a6;
+    a6 = t3;
+    a0 = &a2;
     *t4 = a5;
 
-    return a0;
+    return *a0;
 }
 
 main() {

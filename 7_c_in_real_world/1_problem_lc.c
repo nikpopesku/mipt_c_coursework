196177bc77c44e2423047ece32ae8855b95b9181
Nikolai Popesku <nic1k@yandex.com>
2026-09-08 12:37:45 +0800

    7_c_in_real_world/1_problem_lc.c

--- 7_c_in_real_world/1_problem_lc.c
+++ 7_c_in_real_world/1_problem_lc.c
@@ -4,5 +4,12 @@
 #include <string.h>
 
 int main() {
+    unsigned cell_count, queries_count;
+    int res = scanf("%u", &cell_count);
+    assert(res == 1);
+
+    res =  scanf("%u", &queries_count);
+    assert(res == 1);
+
     return 0;
 }

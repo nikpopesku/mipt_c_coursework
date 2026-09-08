2614295a22cebdcab688ec26911ffc08a4fd0e30
Nikolai Popesku <nic1k@yandex.com>
2026-09-08 12:39:51 +0800

    7_c_in_real_world/1_problem_lc.c

--- 7_c_in_real_world/1_problem_lc.c
+++ 7_c_in_real_world/1_problem_lc.c
@@ -4,12 +4,18 @@
 #include <string.h>
 
 int main() {
-    unsigned cell_count, queries_count;
+    unsigned cell_count, queries_count, i;
+    int number;
     int res = scanf("%u", &cell_count);
     assert(res == 1);
 
-    res =  scanf("%u", &queries_count);
+    res = scanf("%u", &queries_count);
     assert(res == 1);
+
+    for (i = 0; i < queries_count; ++i) {
+        res = scanf("%d", &number);
+        assert(res == 1);
+    }
 
     return 0;
 }
